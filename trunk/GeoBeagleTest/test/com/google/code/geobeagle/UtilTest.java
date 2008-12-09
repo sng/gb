de8d20dd829e9ecc62537c5c055fa5a790c00101
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-09 06:37:37 +0000

    Add UtilTest.java
    git-svn-id: https://geobeagle.googlecode.com/svn@21 eab1e41a-c3b7-11dd-b16b-519e865567fa

---
+++ trunk/GeoBeagleTest/test/com/google/code/geobeagle/UtilTest.java
@@ -0,0 +1,70 @@
+package com.google.code.geobeagle;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
+import junit.framework.TestCase;
+import android.net.UrlQuerySanitizer;
+import android.net.UrlQuerySanitizer.ValueSanitizer;
+
+import com.google.code.geobeagle.Util;
+
+public class UtilTest extends TestCase {
+	public void testConvert() {
+		assertEquals(122.0, Util.minutesToDegrees("122"));
+		assertEquals(122.5, Util.minutesToDegrees("122 30"));
+		assertEquals(122.51, Util.minutesToDegrees("122 30.600"));
+		assertEquals(-122.51, Util.minutesToDegrees("-122 30.600"));
+		assertEquals(-122.51, Util.minutesToDegrees("-122¡ 30.600"));
+		assertEquals(122.51, Util.minutesToDegrees("E 122¡ 30.600"));
+		assertEquals(-122.51, Util.minutesToDegrees("W 122¡ 30.600"));
+		assertEquals(-37.0, Util.minutesToDegrees("S 37¡ 0.000"));
+		assertEquals(-37.0, Util.minutesToDegrees(" S 37¡ 0.000"));
+	}
+
+	public void testGetLatLonFromQueryGCAtEnd() {
+		// Util.getLatLonFromQuery("a");
+		String[] ll = Util.getLatLonFromQuery("N+47¡+40.464+W+122¡+20.119+(GCTANE)+");
+		assertEquals("N 47¡ 40.464", ll[0]);
+		assertEquals("W 122¡ 20.119", ll[1]);
+		assertEquals("GCTANE", ll[2]);
+	}
+
+	public void testGetLatLonFromQuery() {
+		String[] latLonFromQuery = Util.getLatLonFromQuery("GC1ERCC@N+37¡+15.165+W+122¡+02.620+");
+		assertEquals("N 37¡ 15.165", latLonFromQuery[0]);
+		assertEquals("W 122¡ 02.620", latLonFromQuery[1]);
+		latLonFromQuery = Util.getLatLonFromQuery("GC1@N+37¡+15.165+W+122¡+02.620+");
+		assertEquals("N 37¡ 15.165", latLonFromQuery[0]);
+		assertEquals("W 122¡ 02.620", latLonFromQuery[1]);
+
+		latLonFromQuery = Util.getLatLonFromQuery("GC1@N+37¡+15.165+E+122¡+02.620+");
+		assertEquals("N 37¡ 15.165", latLonFromQuery[0]);
+		assertEquals("E 122¡ 02.620", latLonFromQuery[1]);
+
+		latLonFromQuery = Util.getLatLonFromQuery("GC1@N+3¡+15.165+E+122¡+02.620+");
+		assertEquals("N 3¡ 15.165", latLonFromQuery[0]);
+		assertEquals("E 122¡ 02.620", latLonFromQuery[1]);
+	}
+
+	public void testParseHttpUri() {
+		UrlQuerySanitizer sanitizer = createMock(UrlQuerySanitizer.class);
+		ValueSanitizer valueSanitizer = createMock(ValueSanitizer.class);
+		sanitizer.registerParameters(Util.geocachingQueryParam, valueSanitizer);
+		final String unsanitizedQuery = "#unsanitized_query";
+		sanitizer.parseQuery(unsanitizedQuery);
+		final String sanitizedQuery = "#sanitized_query";
+		expect(sanitizer.getValue("q")).andReturn(sanitizedQuery);
+
+		replay(sanitizer);
+		assertEquals(sanitizedQuery, Util.parseHttpUri(unsanitizedQuery, sanitizer, valueSanitizer));
+		verify(sanitizer);
+	}
+
+	public void testConvertDegtreesToMinutes() {
+		assertEquals("-122 30.000", Util.degreesToMinutes(-122.5));
+		assertEquals("-122 30.600", Util.degreesToMinutes(-122.51));
+		assertEquals("-122 03.000", Util.degreesToMinutes(-122.05));
+	}
+
+}

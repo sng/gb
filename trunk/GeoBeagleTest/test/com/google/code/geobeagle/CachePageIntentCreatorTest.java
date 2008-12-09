3a805444b5285b7d391b3bd1ebf0d12cd3f9a6fc
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-09 05:40:14 +0000

    Check in a real test.
    git-svn-id: https://geobeagle.googlecode.com/svn@6 eab1e41a-c3b7-11dd-b16b-519e865567fa

---
+++ trunk/GeoBeagleTest/test/com/google/code/geobeagle/CachePageIntentCreatorTest.java
@@ -0,0 +1,31 @@
+package com.google.code.geobeagle;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
+import junit.framework.TestCase;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.android.geobrowse.CachePageIntentCreator;
+import com.android.geobrowse.Destination;
+import com.android.geobrowse.UriParser;
+
+public class CachePageIntentCreatorTest extends TestCase {
+	public void test() {
+		UriParser uriParser = createMock(UriParser.class);
+		Uri uri = createMock(Uri.class);
+		Intent intent = createMock(Intent.class);
+
+		expect(uriParser.createIntent(Intent.ACTION_VIEW, uri)).andReturn(intent);
+		expect(uriParser.parse("http://coord.info/GCFOO")).andReturn(uri);
+
+		replay(uriParser);
+		replay(intent);
+		CachePageIntentCreator cpic = new CachePageIntentCreator(uriParser);
+		assertEquals(cpic.createIntent(new Destination("37 12.234 122 56.789 # GCFOO")), intent);
+		verify(uriParser);
+		verify(intent);
+	}
+}

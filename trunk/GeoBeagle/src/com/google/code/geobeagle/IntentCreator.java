696457d4145becd110f82aacaa4c9cd2af55059a
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-09 06:05:18 +0000

    Add CachePageIntentCreator and dependencies.
    git-svn-id: https://geobeagle.googlecode.com/svn@9 eab1e41a-c3b7-11dd-b16b-519e865567fa

---
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/IntentCreator.java
@@ -0,0 +1,5 @@
+package com.google.code.geobeagle;
+
+public interface IntentCreator {
+
+}
---
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/UriParserImpl.java
@@ -0,0 +1,18 @@
+package com.google.code.geobeagle;
+
+import android.content.Intent;
+import android.net.Uri;
+
+public class UriParserImpl implements UriParser {
+
+	public Intent createIntent(String actionView, Uri parse) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Uri parse(String format) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

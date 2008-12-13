717c8abf4c3e5340dad4d9500d6575182d5c2023
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-13 15:03:35 +0000

    Submit tests for last change.
    git-svn-id: https://geobeagle.googlecode.com/svn@30 eab1e41a-c3b7-11dd-b16b-519e865567fa

---
+++ trunk/GeoBeagleTest/test/com/google/code/geobeagle/MockableTextViewTest.java
@@ -0,0 +1,10 @@
+package com.google.code.geobeagle;
+
+import junit.framework.TestCase;
+import android.widget.TextView;
+
+public class MockableTextViewTest extends TestCase {
+	public void testConstruct() {
+		MockableTextView mtv = new MockableTextView(null);
+	}
+}

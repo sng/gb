dd77ab9d2c8e0c03f4477915d3f45a21be6dfc45
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-16 01:18:46 +0000

    Reformatting.
    git-svn-id: https://geobeagle.googlecode.com/svn@47 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagle/src/com/google/code/geobeagle/GpsLocationListener.java
+++ branches/sng/GeoBeagle/src/com/google/code/geobeagle/GpsLocationListener.java
@@ -6,16 +6,16 @@ import android.location.LocationListener;
 import android.os.Bundle;
 
 public final class GpsLocationListener implements LocationListener {
-//	private AlertDialog dlgGpsDisabled;
+	// private AlertDialog dlgGpsDisabled;
 	private LocationViewer locationViewer;
 
 	public GpsLocationListener(LocationViewer locationViewer, Context context) {
 		this.locationViewer = locationViewer;
-//		Builder builder = new AlertDialog.Builder(context);
-//		builder.setIcon(android.R.drawable.ic_dialog_alert);
-//		builder.setTitle("GPS DISABLED!");
-//		builder.setMessage("Please enable it in Settings / Security & Location.");
-//		dlgGpsDisabled = builder.create();
+		// Builder builder = new AlertDialog.Builder(context);
+		// builder.setIcon(android.R.drawable.ic_dialog_alert);
+		// builder.setTitle("GPS DISABLED!");
+		// builder.setMessage("Please enable it in Settings / Security & Location.");
+		// dlgGpsDisabled = builder.create();
 	}
 
 	public void onLocationChanged(Location location) {
@@ -23,11 +23,11 @@ public final class GpsLocationListener implements LocationListener {
 	}
 
 	public void onProviderDisabled(String provider) {
-//		dlgGpsDisabled.show();
+		// dlgGpsDisabled.show();
 	}
 
 	public void onProviderEnabled(String provider) {
-//		dlgGpsDisabled.hide();
+		// dlgGpsDisabled.hide();
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {

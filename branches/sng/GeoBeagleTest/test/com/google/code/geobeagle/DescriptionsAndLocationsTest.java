03391af3166e3a3b027b71728218563d1b1c2550
stephen5.ng@gmail.com <stephen5.ng@gmail.com@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2008-12-16 07:07:12 +0000

    Format according to Google Android standard (development/ide/eclipse/...).
    git-svn-id: https://geobeagle.googlecode.com/svn@51 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/DescriptionsAndLocationsTest.java
+++ branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/DescriptionsAndLocationsTest.java
@@ -1,60 +1,69 @@
+
 package com.google.code.geobeagle;
 
 import junit.framework.TestCase;
 
 public class DescriptionsAndLocationsTest extends TestCase {
 
-	public final void testDescriptionsAndLocations() {
-		DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
-		
-		CharSequence[] descriptions = new CharSequence[]{"SFO", "OAK"};
-		final CharSequence[] locations = new CharSequence[]{"37 122 etc", "37 122 foo"};
-		descriptionsAndLocations = new DescriptionsAndLocations();
-		for (int ix = 0; ix < descriptions.length; ix++) {
-			descriptionsAndLocations.add(descriptions[ix], locations[ix]);
-		}
-		assertEquals(descriptions[1], descriptionsAndLocations.getPreviousDescriptions().get(1));
-		assertEquals(locations[0], descriptionsAndLocations.getPreviousLocations().get(0));
-	}
+    public final void testDescriptionsAndLocations() {
+        DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
 
-	public final void testAdd() {
-		DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
-		descriptionsAndLocations.add("new description", "new location");
-		assertEquals("new description", descriptionsAndLocations.getPreviousDescriptions().get(0));
-	}
+        CharSequence[] descriptions = new CharSequence[] {
+                "SFO", "OAK"
+        };
+        final CharSequence[] locations = new CharSequence[] {
+                "37 122 etc", "37 122 foo"
+        };
+        descriptionsAndLocations = new DescriptionsAndLocations();
+        for (int ix = 0; ix < descriptions.length; ix++) {
+            descriptionsAndLocations.add(descriptions[ix], locations[ix]);
+        }
+        assertEquals(descriptions[1], descriptionsAndLocations.getPreviousDescriptions().get(1));
+        assertEquals(locations[0], descriptionsAndLocations.getPreviousLocations().get(0));
+    }
 
-	public final void testAddDupe() {
-		DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
-		descriptionsAndLocations.add("description1", "new location");
-		descriptionsAndLocations.add("description2", "new location");
-		descriptionsAndLocations.add("description2", "new location");
+    public final void testAdd() {
+        DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
+        descriptionsAndLocations.add("new description", "new location");
+        assertEquals("new description", descriptionsAndLocations.getPreviousDescriptions().get(0));
+    }
 
-		assertEquals(2, descriptionsAndLocations.getPreviousDescriptions().size());
-		assertEquals("description1", descriptionsAndLocations.getPreviousDescriptions().get(0));
-		assertEquals("description2", descriptionsAndLocations.getPreviousDescriptions().get(1));
-	}
-	
-	public final void testAddRemoveOldest() {
-		DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations(3);
-		descriptionsAndLocations.add("description1", "new location");
-		descriptionsAndLocations.add("description2", "new location");
-		descriptionsAndLocations.add("description3", "new location");
-		descriptionsAndLocations.add("description4", "new location");
+    public final void testAddDupe() {
+        DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
+        descriptionsAndLocations.add("description1", "new location");
+        descriptionsAndLocations.add("description2", "new location");
+        descriptionsAndLocations.add("description2", "new location");
 
-		assertEquals(3, descriptionsAndLocations.getPreviousDescriptions().size());
-		assertEquals("description2", descriptionsAndLocations.getPreviousDescriptions().get(0));
-	}
-	
-	
-	public final void testClear() {
-		CharSequence[] descriptions = new CharSequence[]{"SFO", "OAK"};
-		final CharSequence[] locations = new CharSequence[]{"37 122 etc", "37 122 foo"};
-		DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
-		for (int ix = 0; ix < descriptions.length; ix++) {
-			descriptionsAndLocations.add(descriptions[ix], locations[ix]);
-		}		descriptionsAndLocations.clear();
-		
-		assertEquals(0, descriptionsAndLocations.getPreviousDescriptions().size());
-		assertEquals(0, descriptionsAndLocations.getPreviousLocations().size());
-	}
+        assertEquals(2, descriptionsAndLocations.getPreviousDescriptions().size());
+        assertEquals("description1", descriptionsAndLocations.getPreviousDescriptions().get(0));
+        assertEquals("description2", descriptionsAndLocations.getPreviousDescriptions().get(1));
+    }
+
+    public final void testAddRemoveOldest() {
+        DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations(3);
+        descriptionsAndLocations.add("description1", "new location");
+        descriptionsAndLocations.add("description2", "new location");
+        descriptionsAndLocations.add("description3", "new location");
+        descriptionsAndLocations.add("description4", "new location");
+
+        assertEquals(3, descriptionsAndLocations.getPreviousDescriptions().size());
+        assertEquals("description2", descriptionsAndLocations.getPreviousDescriptions().get(0));
+    }
+
+    public final void testClear() {
+        CharSequence[] descriptions = new CharSequence[] {
+                "SFO", "OAK"
+        };
+        final CharSequence[] locations = new CharSequence[] {
+                "37 122 etc", "37 122 foo"
+        };
+        DescriptionsAndLocations descriptionsAndLocations = new DescriptionsAndLocations();
+        for (int ix = 0; ix < descriptions.length; ix++) {
+            descriptionsAndLocations.add(descriptions[ix], locations[ix]);
+        }
+        descriptionsAndLocations.clear();
+
+        assertEquals(0, descriptionsAndLocations.getPreviousDescriptions().size());
+        assertEquals(0, descriptionsAndLocations.getPreviousLocations().size());
+    }
 }
--- branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/LocationViewerImplTest.java
+++ branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/LocationViewerImplTest.java
@@ -1,32 +1,35 @@
+
 package com.google.code.geobeagle;
 
 import static org.easymock.EasyMock.expect;
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
-import junit.framework.TestCase;
+
 import android.location.Location;
+
+import junit.framework.TestCase;
 
 public class LocationViewerImplTest extends TestCase {
 
-	public final void testSetLocationLocation() {
-		Location location = createMock(Location.class);
-		expect(location.getLatitude()).andReturn(37.0);
-		expect(location.getLongitude()).andReturn(122.0);
-		expect(location.getTime()).andReturn(300L);
-		MockableButton button = createMock(MockableButton.class);
-		button.setEnabled(false);
-		button.setEnabled(true);
-		button.setText("GPS@16:00:00");
-		MockableTextView coordinates = createMock(MockableTextView.class);
-		coordinates.setText("37 00.000 122 00.000");
-		replay(location);
-		replay(button);
-		replay(coordinates);
-		new LocationViewerImpl(button, coordinates, location);
-		verify(location);
-		verify(button);
-		verify(coordinates);
-	}
+    public final void testSetLocationLocation() {
+        Location location = createMock(Location.class);
+        expect(location.getLatitude()).andReturn(37.0);
+        expect(location.getLongitude()).andReturn(122.0);
+        expect(location.getTime()).andReturn(300L);
+        MockableButton button = createMock(MockableButton.class);
+        button.setEnabled(false);
+        button.setEnabled(true);
+        button.setText("GPS@16:00:00");
+        MockableTextView coordinates = createMock(MockableTextView.class);
+        coordinates.setText("37 00.000 122 00.000");
+        replay(location);
+        replay(button);
+        replay(coordinates);
+        new LocationViewerImpl(button, coordinates, location);
+        verify(location);
+        verify(button);
+        verify(coordinates);
+    }
 
 }

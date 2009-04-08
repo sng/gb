8642471ee4f8b6a5efb827b5eb618f2a5a2a5dd9
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-04-08 16:48:19 +0000

    powermock
    git-svn-id: https://geobeagle.googlecode.com/svn@310 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/io/CacheReaderTest.java
+++ branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/io/CacheReaderTest.java
@@ -4,9 +4,7 @@ package com.google.code.geobeagle.io;
 import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isNull;
-import static org.easymock.classextension.EasyMock.createMock;
-import static org.easymock.classextension.EasyMock.replay;
-import static org.easymock.classextension.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
 import com.google.code.geobeagle.data.Geocache;
 import com.google.code.geobeagle.data.GeocacheFactory;
@@ -15,12 +13,16 @@ import com.google.code.geobeagle.io.CacheReader.CacheReaderCursor;
 import com.google.code.geobeagle.io.CacheReader.WhereFactory;
 import com.google.code.geobeagle.io.DatabaseDI.SQLiteWrapper;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import android.database.Cursor;
 import android.location.Location;
 
-import junit.framework.TestCase;
-
-public class CacheReaderTest extends TestCase {
+@RunWith(PowerMockRunner.class)
+public class CacheReaderTest {
 
     private void expectQuery(SQLiteWrapper sqliteWrapper, Cursor cursor, String where) {
         expect(
@@ -29,21 +31,23 @@ public class CacheReaderTest extends TestCase {
                         eq(CacheReader.SQL_QUERY_LIMIT))).andReturn(cursor);
     }
 
+    @Test
     public void testCursorClose() {
-        Cursor cursor = createMock(Cursor.class);
+        Cursor cursor = PowerMock.createMock(Cursor.class);
 
         cursor.close();
 
-        replay(cursor);
+        PowerMock.replayAll();
         new CacheReaderCursor(cursor, null, null).close();
-        verify(cursor);
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testCursorGetCache() {
-        Cursor cursor = createMock(Cursor.class);
-        GeocacheFactory geocacheFactory = createMock(GeocacheFactory.class);
-        Geocache geocache = createMock(Geocache.class);
-        DbToGeocacheAdapter dbToGeocacheAdapter = createMock(DbToGeocacheAdapter.class);
+        Cursor cursor = PowerMock.createMock(Cursor.class);
+        GeocacheFactory geocacheFactory = PowerMock.createMock(GeocacheFactory.class);
+        Geocache geocache = PowerMock.createMock(Geocache.class);
+        DbToGeocacheAdapter dbToGeocacheAdapter = PowerMock.createMock(DbToGeocacheAdapter.class);
 
         expect(cursor.getDouble(0)).andReturn(122.0);
         expect(cursor.getDouble(1)).andReturn(37.0);
@@ -54,57 +58,61 @@ public class CacheReaderTest extends TestCase {
         expect(geocacheFactory.create("GC123", "name", 122.0, 37.0, Source.GPX, "cupertino"))
                 .andReturn(geocache);
 
-        replay(cursor);
-        replay(geocacheFactory);
-        replay(dbToGeocacheAdapter);
-        assertEquals(geocache, new CacheReaderCursor(cursor, geocacheFactory, dbToGeocacheAdapter).getCache());
-        verify(cursor);
-        verify(geocacheFactory);
+        PowerMock.replayAll();
+        assertEquals(geocache, new CacheReaderCursor(cursor, geocacheFactory, dbToGeocacheAdapter)
+                .getCache());
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testCursorMoveToNext() {
-        Cursor cursor = createMock(Cursor.class);
+        Cursor cursor = PowerMock.createMock(Cursor.class);
 
         expect(cursor.moveToNext()).andReturn(true);
 
-        replay(cursor);
+        PowerMock.replayAll();
         new CacheReaderCursor(cursor, null, null).moveToNext();
-        verify(cursor);
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testGetTotalCount() {
-        SQLiteWrapper sqliteWrapper = createMock(SQLiteWrapper.class);
+        SQLiteWrapper sqliteWrapper = PowerMock.createMock(SQLiteWrapper.class);
 
         expect(sqliteWrapper.countResults(Database.TBL_CACHES, null)).andReturn(17);
 
-        replay(sqliteWrapper);
+        PowerMock.replayAll();
         assertEquals(17, new CacheReader(sqliteWrapper, null, null).getTotalCount());
-        verify(sqliteWrapper);
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testGetWhere() {
-        Location location = createMock(Location.class);
+        Location location = PowerMock.createMock(Location.class);
         expect(location.getLatitude()).andReturn(90.0);
         expect(location.getLongitude()).andReturn(180.0);
 
-        replay(location);
+        PowerMock.replayAll();
         assertEquals(
                 "Latitude > 89.92 AND Latitude < 90.08 AND Longitude > -180.0 AND Longitude < 180.0",
                 new WhereFactory().getWhere(location));
-        verify(location);
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testGetWhereNullLocation() {
         assertEquals(null, new WhereFactory().getWhere(null));
     }
 
+    @Test
     public void testOpen() {
-        Location location = createMock(Location.class);
-        WhereFactory whereFactory = createMock(WhereFactory.class);
-        SQLiteWrapper sqliteWrapper = createMock(SQLiteWrapper.class);
-        Cursor cursor = createMock(Cursor.class);
-        DatabaseDI.CacheReaderCursorFactory cacheReaderCursorFactory = createMock(DatabaseDI.CacheReaderCursorFactory.class);
-        CacheReaderCursor cacheReaderCursor = createMock(CacheReaderCursor.class);
+        Location location = PowerMock.createMock(Location.class);
+        WhereFactory whereFactory = PowerMock.createMock(WhereFactory.class);
+        SQLiteWrapper sqliteWrapper = PowerMock.createMock(SQLiteWrapper.class);
+        Cursor cursor = PowerMock.createMock(Cursor.class);
+        DatabaseDI.CacheReaderCursorFactory cacheReaderCursorFactory = PowerMock
+                .createMock(DatabaseDI.CacheReaderCursorFactory.class);
+        CacheReaderCursor cacheReaderCursor = PowerMock.createMock(CacheReaderCursor.class);
 
         String where = "Latitude > something AND Longitude < somethingelse";
         expect(whereFactory.getWhere(location)).andReturn(where);
@@ -112,35 +120,25 @@ public class CacheReaderTest extends TestCase {
         expect(cursor.moveToFirst()).andReturn(true);
         expect(cacheReaderCursorFactory.create(cursor)).andReturn(cacheReaderCursor);
 
-        replay(whereFactory);
-        replay(sqliteWrapper);
-        replay(cursor);
-        replay(cacheReaderCursorFactory);
+        PowerMock.replayAll();
         assertEquals(cacheReaderCursor, new CacheReader(sqliteWrapper, whereFactory,
                 cacheReaderCursorFactory).open(location));
-        verify(whereFactory);
-        verify(sqliteWrapper);
-        verify(cursor);
-        verify(cacheReaderCursorFactory);
-
+        PowerMock.verifyAll();
     }
 
+    @Test
     public void testOpenEmpty() {
-        SQLiteWrapper sqliteWrapper = createMock(SQLiteWrapper.class);
-        Cursor cursor = createMock(Cursor.class);
-        WhereFactory whereFactory = createMock(WhereFactory.class);
+        SQLiteWrapper sqliteWrapper = PowerMock.createMock(SQLiteWrapper.class);
+        Cursor cursor = PowerMock.createMock(Cursor.class);
+        WhereFactory whereFactory = PowerMock.createMock(WhereFactory.class);
 
         expect(whereFactory.getWhere(null)).andReturn("a=b");
         expectQuery(sqliteWrapper, cursor, "a=b");
         expect(cursor.moveToFirst()).andReturn(false);
         cursor.close();
 
-        replay(whereFactory);
-        replay(sqliteWrapper);
-        replay(cursor);
+        PowerMock.replayAll();
         new CacheReader(sqliteWrapper, whereFactory, null).open(null);
-        verify(sqliteWrapper);
-        verify(cursor);
-        verify(whereFactory);
+        PowerMock.verifyAll();
     }
 }

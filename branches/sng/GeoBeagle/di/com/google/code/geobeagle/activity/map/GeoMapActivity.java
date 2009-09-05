0a43c699a91ac282b7e9498bf607a6445fdd0c5d
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-09-05 05:38:36 +0000

    add tests and refactor
    git-svn-id: https://geobeagle.googlecode.com/svn@602 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagle/di/com/google/code/geobeagle/activity/map/GeoMapActivity.java
+++ branches/sng/GeoBeagle/di/com/google/code/geobeagle/activity/map/GeoMapActivity.java
@@ -1,14 +1,40 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
 
 package com.google.code.geobeagle.activity.map;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
 import com.google.code.geobeagle.R;
+import com.google.code.geobeagle.database.DatabaseDI;
+import com.google.code.geobeagle.database.GeocachesSql;
+import com.google.code.geobeagle.database.ISQLiteDatabase;
+import com.google.code.geobeagle.database.WhereFactoryNearestCaches;
 import com.google.code.geobeagle.database.DatabaseDI.GeoBeagleSqliteOpenHelper;
+import com.google.code.geobeagle.database.DatabaseDI.SearchFactory;
+import com.google.code.geobeagle.database.WhereFactoryNearestCaches.WhereStringFactory;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.List;
 
 public class GeoMapActivity extends MapActivity {
     GeoMapActivityDelegate mGeoMapActivityDelegate;
@@ -23,10 +49,26 @@ public class GeoMapActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
-        MapView mapView = (MapView)findViewById(R.id.mapview);
-        mGeoMapActivityDelegate = new GeoMapActivityDelegate(this, mapView, getApplicationContext());
-        GeoBeagleSqliteOpenHelper open = new GeoBeagleSqliteOpenHelper(this);
-        mGeoMapActivityDelegate.onCreate(savedInstanceState, open);
+        final MapView mapView = (MapView)findViewById(R.id.mapview);
+        final GeoBeagleSqliteOpenHelper open = new GeoBeagleSqliteOpenHelper(this);
+        final SQLiteDatabase sqDb = open.getReadableDatabase();
+        final ISQLiteDatabase database = new DatabaseDI.SQLiteWrapper(sqDb);
+        final GeocachesSql geocachesSql = DatabaseDI.createGeocachesSql(database);
+        final WhereStringFactory whereStringFactory = new WhereStringFactory();
+        final SearchFactory searchFactory = new SearchFactory();
+        final WhereFactoryNearestCaches whereFactory = new WhereFactoryNearestCaches(searchFactory,
+                whereStringFactory);
+        final Drawable drawable = getResources().getDrawable(R.drawable.map_others);
+        final MapItemizedOverlay mapItemizedOverlay = new MapItemizedOverlay(this, drawable);
+        final MyLocationOverlay myLocationOverlay = new MyLocationOverlay(this, mapView);
+        final MapController mapController = mapView.getController();
+        final List<Overlay> mapOverlays = mapView.getOverlays();
+
+        mGeoMapActivityDelegate = new GeoMapActivityDelegate(this, mapView,
+                getApplicationContext(), myLocationOverlay);
+        mGeoMapActivityDelegate.initialize(geocachesSql, whereFactory, mapItemizedOverlay,
+                mapController, mapOverlays);
+        open.close();
     }
 
     @Override
@@ -48,7 +90,6 @@ public class GeoMapActivity extends MapActivity {
         // return mCacheListDelegate.onCreateOptionsMenu(menu);
 
         return true;
-
     }
 
     @Override
--- branches/sng/GeoBeagle/src/com/google/code/geobeagle/activity/map/GeoMapActivityDelegate.java
+++ branches/sng/GeoBeagle/src/com/google/code/geobeagle/activity/map/GeoMapActivityDelegate.java
@@ -1,3 +1,16 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
 
 package com.google.code.geobeagle.activity.map;
 
@@ -6,79 +19,90 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
-import com.google.code.geobeagle.Geocache;
 import com.google.code.geobeagle.R;
+import com.google.code.geobeagle.activity.MenuAction;
 import com.google.code.geobeagle.activity.cachelist.CacheList;
 import com.google.code.geobeagle.activity.main.GeoUtils;
-import com.google.code.geobeagle.database.DatabaseDI;
 import com.google.code.geobeagle.database.GeocachesSql;
-import com.google.code.geobeagle.database.ISQLiteDatabase;
 import com.google.code.geobeagle.database.WhereFactoryNearestCaches;
-import com.google.code.geobeagle.database.DatabaseDI.GeoBeagleSqliteOpenHelper;
-import com.google.code.geobeagle.database.DatabaseDI.SearchFactory;
-import com.google.code.geobeagle.database.WhereFactoryNearestCaches.WhereStringFactory;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GeoMapActivityDelegate {
-    private Activity mParent;
-    private MapView mMapView;
-    private MapController mController;
-    private Context mContext;
-    private boolean mIsSatelliteView = false;
+    static class MenuActionCacheList implements MenuAction {
+        private final Activity mActivity;
 
-    private List<Overlay> mMapOverlays;
-    private MapItemizedOverlay mCachesOverlay;
-    private MyLocationOverlay mMyLocationOverlay;
+        MenuActionCacheList(Activity activity) {
+            mActivity = activity;
+        }
 
-    public GeoMapActivityDelegate(Activity parent, MapView mapView, Context context) {
-        mParent = parent;
-        mMapView = mapView;
-        mController = mapView.getController();
-        mContext = context;
+        @Override
+        public void act() {
+            mActivity.startActivity(new Intent(mActivity, CacheList.class));
+        }
     }
 
-    public void onCreate(Bundle savedInstanceState, GeoBeagleSqliteOpenHelper open) {
-        Intent i = mParent.getIntent();
-        double latitude = i.getDoubleExtra("latitude", 0);
-        double longitude = i.getDoubleExtra("longitude", 0);
+    public static class MenuActionToggleSatellite implements MenuAction {
+        private final MapView mMapView;
+
+        public MenuActionToggleSatellite(MapView mapView) {
+            mMapView = mapView;
+        }
+
+        @Override
+        public void act() {
+            mMapView.setSatellite(!mMapView.isSatellite());
+        }
+    }
+
+    private final Context mContext;
+    private final MapView mMapView;
+    private final MenuActions mMenuActions;
+    private final MyLocationOverlay mMyLocationOverlay;
+
+    public GeoMapActivityDelegate(Activity parent, MapView mapView, Context context,
+            MyLocationOverlay myLocationOverlay, MenuActions menuActions) {
+        mContext = context;
+        mMapView = mapView;
+        mMyLocationOverlay = myLocationOverlay;
+        mMenuActions = menuActions;
+    }
+
+    public void initialize(Intent intent, GeocachesSql geocachesSql,
+            WhereFactoryNearestCaches whereFactory, MapItemizedOverlay cachesOverlay,
+            MapController mapController, List<Overlay> mapOverlays) {
         mMapView.setBuiltInZoomControls(true);
-
         // mMapView.setOnLongClickListener()
-        mMapView.setSatellite(mIsSatelliteView);
-        mController = mMapView.getController();
+        mMapView.setSatellite(false);
 
-        // mController.setZoom(14);
+        double latitude = intent.getFloatExtra("latitude", 0);
+        double longitude = intent.getFloatExtra("longitude", 0);
         GeoPoint center = new GeoPoint((int)(latitude * GeoUtils.MILLION),
                 (int)(longitude * GeoUtils.MILLION));
 
-        mController.setCenter(center);
-        mMapOverlays = mMapView.getOverlays();
+        mapController.setCenter(center);
+        // controller.setZoom(14);
 
-        Drawable drawable = mContext.getResources().getDrawable(R.drawable.map_others);
-        mCachesOverlay = new MapItemizedOverlay(mContext, drawable);
-        mCachesOverlay.doPopulate();
+        mapOverlays.add(cachesOverlay);
+        mapOverlays.add(mMyLocationOverlay);
 
-        mMapOverlays.add(mCachesOverlay);
-        mMyLocationOverlay = new MyLocationOverlay(mContext, mMapView);
-        mMapOverlays.add(mMyLocationOverlay);
-        
-        refreshCaches(latitude, longitude, open);
+        cachesOverlay.addCaches(mContext, latitude, longitude, geocachesSql, whereFactory);
     }
 
-    public void onResume() {
-        mMyLocationOverlay.enableMyLocation();
-        mMyLocationOverlay.enableCompass();
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        menu.findItem(R.id.menu_toggle_satellite).setTitle(
+                mMapView.isSatellite() ? R.string.satellite_view : R.string.map_view);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mMenuActions.act(item.getItemId());
     }
 
     public void onPause() {
@@ -86,49 +110,8 @@ public class GeoMapActivityDelegate {
         mMyLocationOverlay.disableCompass();
     }
 
-    public boolean onMenuOpened(int featureId, Menu menu) {
-        String satStr;
-        if (mIsSatelliteView)
-            satStr = "Map view";
-        else
-            satStr = "Satellite view";
-
-        menu.findItem(R.id.menu_toggle_satellite).setTitle(satStr);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_toggle_satellite) {
-            mIsSatelliteView = !mIsSatelliteView;
-            mMapView.setSatellite(mIsSatelliteView);
-            return true;
-        }
-
-        if (item.getItemId() == R.id.menu_cache_list) {
-            mParent.startActivity(new Intent(mContext, CacheList.class));
-            return true;
-        }
-
-        return false;
-    }
-
-    public void refreshCaches(double latitude, double longitude, GeoBeagleSqliteOpenHelper open) {
-        mCachesOverlay.clearOverlays();
-        SQLiteDatabase sqDb = open.getReadableDatabase();
-        ISQLiteDatabase database = new DatabaseDI.SQLiteWrapper(sqDb);
-        GeocachesSql sql = DatabaseDI.createGeocachesSql(database);
-        WhereStringFactory whereStringFactory = new WhereStringFactory();
-        SearchFactory searchFactory = new SearchFactory();
-        sql.loadCaches(latitude, longitude, new WhereFactoryNearestCaches(searchFactory,
-                whereStringFactory));
-        ArrayList<Geocache> list = sql.getGeocaches();
-
-        for (Geocache cache : list) {
-            CacheItem item = CacheItem.Create(mContext.getResources(), cache);
-            if (item != null)
-                mCachesOverlay.addOverlay(item);
-        }
-        open.close();
-        mCachesOverlay.doPopulate();
+    public void onResume() {
+        mMyLocationOverlay.enableMyLocation();
+        mMyLocationOverlay.enableCompass();
     }
 }

b52373699b6092103976d774c224f764baeb0fcc
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-09-06 17:12:08 +0000

    Merged trunk changed r629:646 to sng branch.
    git-svn-id: https://geobeagle.googlecode.com/svn@647 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/GeoMapActivityDelegateTest.java
+++ branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/GeoMapActivityDelegateTest.java
@@ -14,19 +14,6 @@
 
 package com.google.code.geobeagle.activity.map;
 
-import static org.junit.Assert.assertEquals;
-
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.google.android.maps.MyLocationOverlay;
-import com.google.android.maps.Overlay;
-import com.google.code.geobeagle.R;
-import com.google.code.geobeagle.activity.MenuActions;
-import com.google.code.geobeagle.activity.cachelist.CacheList;
-import com.google.code.geobeagle.database.GeocachesSql;
-import com.google.code.geobeagle.database.WhereFactoryNearestCaches;
-
 import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,13 +22,17 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.code.geobeagle.R;
+import com.google.code.geobeagle.activity.MenuActions;
+import com.google.code.geobeagle.activity.cachelist.CacheList;
+import com.google.code.geobeagle.database.GeocachesLoader;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest( {
@@ -50,19 +41,13 @@ import java.util.List;
 public class GeoMapActivityDelegateTest {
     @Test
     public void testInitialize() throws Exception {
-        MapView mapView = PowerMock.createMock(MapView.class);
-        Context context = PowerMock.createMock(Context.class);
-        MyLocationOverlay myLocationOverlay = PowerMock.createMock(MyLocationOverlay.class);
-        GeoMapActivity geoMapActivity = PowerMock.createMock(GeoMapActivity.class);
-        GeocachesSql geocachesSql = PowerMock.createMock(GeocachesSql.class);
-        WhereFactoryNearestCaches whereFactory = PowerMock
-                .createMock(WhereFactoryNearestCaches.class);
-        MapItemizedOverlay cachesOverlay = PowerMock.createMock(MapItemizedOverlay.class);
+        GeoMapView mapView = PowerMock.createMock(GeoMapView.class);
+        GeocachesLoader geocachesLoader = PowerMock.createMock(GeocachesLoader.class);
+        CachePinsOverlay cachesOverlay = PowerMock.createMock(CachePinsOverlay.class);
         Intent intent = PowerMock.createMock(Intent.class);
         GeoPoint geoPoint = PowerMock.createMock(GeoPoint.class);
         MapController mapController = PowerMock.createMock(MapController.class);
 
-        List<Overlay> mapOverlays = new ArrayList<Overlay>();
         mapView.setBuiltInZoomControls(true);
         mapView.setSatellite(false);
         EasyMock.expect(intent.getFloatExtra("latitude", 0)).andReturn(122.0f);
@@ -70,14 +55,14 @@ public class GeoMapActivityDelegateTest {
         PowerMock.expectNew(GeoPoint.class, 122000000, 37000000).andReturn(geoPoint);
         mapController.setCenter(geoPoint);
         EasyMock.expect(mapController.setZoom(14)).andReturn(14);
-        cachesOverlay.addCaches(context, 122, 37, geocachesSql, whereFactory);
-
         PowerMock.replayAll();
+        /*
         new GeoMapActivityDelegate(geoMapActivity, mapView, context, myLocationOverlay, null)
-                .initialize(intent, geocachesSql, whereFactory, cachesOverlay, mapController,
-                        mapOverlays);
-        assertEquals(cachesOverlay, mapOverlays.get(0));
-        assertEquals(myLocationOverlay, mapOverlays.get(1));
+        .initialize(intent, geocachesSql, whereFactory, cachesOverlay, mapController,
+                mapOverlays, cacheItemFactory);
+                */
+        new GeoMapActivityDelegate(mapView, null)
+                .initialize(intent, geocachesLoader, cachesOverlay, mapController);
         PowerMock.verifyAll();
     }
 
@@ -85,7 +70,7 @@ public class GeoMapActivityDelegateTest {
     public void testOnMenuOpened() {
         Menu menu = PowerMock.createMock(Menu.class);
         MenuItem menuItem = PowerMock.createMock(MenuItem.class);
-        MapView mapView = PowerMock.createMock(MapView.class);
+        GeoMapView mapView = PowerMock.createMock(GeoMapView.class);
 
         EasyMock.expect(mapView.isSatellite()).andReturn(false);
         EasyMock.expect(menu.findItem(R.id.menu_toggle_satellite)).andReturn(menuItem);
@@ -97,7 +82,7 @@ public class GeoMapActivityDelegateTest {
 
         PowerMock.replayAll();
         final GeoMapActivityDelegate geoMapActivityDelegate = new GeoMapActivityDelegate(null,
-                mapView, null, null, null);
+                null);
         geoMapActivityDelegate.onMenuOpened(0, menu);
         geoMapActivityDelegate.onMenuOpened(0, menu);
         PowerMock.verifyAll();
@@ -112,32 +97,8 @@ public class GeoMapActivityDelegateTest {
         EasyMock.expect(menuActions.act(12)).andReturn(true);
 
         PowerMock.replayAll();
-        new GeoMapActivityDelegate(null, null, null, null, menuActions)
+        new GeoMapActivityDelegate(null, menuActions)
                 .onOptionsItemSelected(menuItem);
-        PowerMock.verifyAll();
-    }
-
-    @Test
-    public void testOnPause() {
-        MyLocationOverlay myLocationOverlay = PowerMock.createMock(MyLocationOverlay.class);
-
-        myLocationOverlay.disableCompass();
-        myLocationOverlay.disableMyLocation();
-
-        PowerMock.replayAll();
-        new GeoMapActivityDelegate(null, null, null, myLocationOverlay, null).onPause();
-        PowerMock.verifyAll();
-    }
-
-    @Test
-    public void testOnResume() {
-        MyLocationOverlay myLocationOverlay = PowerMock.createMock(MyLocationOverlay.class);
-
-        EasyMock.expect(myLocationOverlay.enableCompass()).andReturn(true);
-        EasyMock.expect(myLocationOverlay.enableMyLocation()).andReturn(true);
-
-        PowerMock.replayAll();
-        new GeoMapActivityDelegate(null, null, null, myLocationOverlay, null).onResume();
         PowerMock.verifyAll();
     }
 

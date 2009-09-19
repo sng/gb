b432fd852dd355a3d27b8f58fec80ef4bb1b0a6d
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-09-19 22:14:12 +0000

    adjust imports
    git-svn-id: https://geobeagle.googlecode.com/svn@707 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/QueryManagerTest.java
+++ branches/sng/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/QueryManagerTest.java
@@ -14,8 +14,9 @@
 
 package com.google.code.geobeagle.activity.map;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;

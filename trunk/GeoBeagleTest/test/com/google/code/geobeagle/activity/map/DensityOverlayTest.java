0f86331420fc9ba6e0bd5c04896aa0dfb233a839
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-09-19 04:59:07 +0000

    test fixes
    git-svn-id: https://geobeagle.googlecode.com/svn@700 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/DensityOverlayTest.java
+++ trunk/GeoBeagleTest/test/com/google/code/geobeagle/activity/map/DensityOverlayTest.java
@@ -42,7 +42,7 @@ public class DensityOverlayTest {
     @Test
     public void testDensityOverlayShadow() {
         DensityOverlayDelegate densityOverlayDelegate = new DensityOverlayDelegate(null, null,
-                null, null, null, null);
+                null, null, null);
         densityOverlayDelegate.draw(null, null, true);
     }
 
@@ -95,7 +95,7 @@ public class DensityOverlayTest {
 
         PowerMock.replayAll();
         DensityOverlayDelegate densityOverlayDelegate = new DensityOverlayDelegate(screenRect,
-                paint, screenTopLeft, screenBottomRight, null, densityPatchManager);
+                paint, screenTopLeft, screenBottomRight, densityPatchManager);
         densityOverlayDelegate.draw(canvas, mapView, false);
         PowerMock.verifyAll();
     }

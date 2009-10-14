307f5684895dd54feca57607e2b7aae5803d4431
andpet <andpet@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-10-14 19:59:57 +0000

    Refactoring: Rename/move ContextAction* to CacheAction*
    git-svn-id: https://geobeagle.googlecode.com/svn@781 eab1e41a-c3b7-11dd-b16b-519e865567fa

---
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/actions/CacheActionDeleteTest.java
@@ -0,0 +1,55 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
+package com.google.code.geobeagle.actions;
+
+import static org.easymock.EasyMock.expect;
+
+import com.google.code.geobeagle.Geocache;
+import com.google.code.geobeagle.actions.CacheActionDelete;
+import com.google.code.geobeagle.activity.cachelist.model.GeocacheVector;
+import com.google.code.geobeagle.activity.cachelist.model.GeocacheVectors;
+import com.google.code.geobeagle.activity.cachelist.presenter.GeocacheListAdapter;
+import com.google.code.geobeagle.activity.cachelist.presenter.TitleUpdater;
+import com.google.code.geobeagle.database.CacheWriter;
+import com.google.code.geobeagle.database.DbFrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class CacheActionDeleteTest {
+
+    @Test
+    public void testActionDelete() {
+        CacheWriter cacheWriter = PowerMock.createMock(CacheWriter.class);
+        GeocacheListAdapter geocacheListAdapter = PowerMock.createMock(GeocacheListAdapter.class);
+        Geocache geocache = PowerMock.createMock(Geocache.class);
+        TitleUpdater titleUpdater = PowerMock.createMock(TitleUpdater.class);
+        DbFrontend dbFrontend = PowerMock.createMock(DbFrontend.class);
+
+        expect(dbFrontend.getCacheWriter()).andReturn(cacheWriter);
+        expect(geocache.getId()).andReturn("GC123");
+        cacheWriter.deleteCache("GC123");
+        geocacheListAdapter.notifyDataSetChanged();
+        titleUpdater.refresh();
+
+        PowerMock.replayAll();
+        new CacheActionDelete(geocacheListAdapter, titleUpdater, dbFrontend)
+                .act(geocache);
+        PowerMock.verifyAll();
+    }
+}
---
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/actions/CacheActionEditTest.java
@@ -0,0 +1,52 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
+package com.google.code.geobeagle.actions;
+
+import com.google.code.geobeagle.Geocache;
+import com.google.code.geobeagle.actions.CacheActionEdit;
+import com.google.code.geobeagle.activity.EditCacheActivity;
+import com.google.code.geobeagle.activity.cachelist.model.GeocacheVector;
+import com.google.code.geobeagle.activity.cachelist.model.GeocacheVectors;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import android.content.Context;
+import android.content.Intent;
+
+@PrepareForTest( {
+    CacheActionEdit.class
+})
+@RunWith(PowerMockRunner.class)
+public class CacheActionEditTest {
+    @Test
+    public void testAct() throws Exception {
+        Context context = PowerMock.createMock(Context.class);
+        Intent intent = PowerMock.createMock(Intent.class);
+        Geocache geocache = PowerMock.createMock(Geocache.class);
+
+        PowerMock.expectNew(Intent.class, context, EditCacheActivity.class).andReturn(intent);
+        EasyMock.expect(intent.putExtra("geocache", geocache)).andReturn(intent);
+        context.startActivity(intent);
+
+        PowerMock.replayAll();
+        new CacheActionEdit(context).act(geocache);
+        PowerMock.verifyAll();
+    }
+}

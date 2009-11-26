538fab7867820643f509c5633cf00bed0cfd096d
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-11-26 17:31:45 +0000

    fix tests.
    git-svn-id: https://geobeagle.googlecode.com/svn@871 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/cachelist/MenuActionMyLocationTest.java
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/cachelist/MenuActionMyLocationTest.java
@@ -14,14 +14,17 @@
 
 package com.google.code.geobeagle.cachelist;
 
-import com.google.code.geobeagle.ErrorDisplayer;
+import com.google.code.geobeagle.CacheType;
+import com.google.code.geobeagle.GeoFix;
+import com.google.code.geobeagle.GeoFixProvider;
 import com.google.code.geobeagle.Geocache;
 import com.google.code.geobeagle.GeocacheFactory;
-import com.google.code.geobeagle.R;
+import com.google.code.geobeagle.GeocacheFactory.Source;
+import com.google.code.geobeagle.actions.CacheAction;
 import com.google.code.geobeagle.activity.cachelist.actions.MenuActionMyLocation;
-import com.google.code.geobeagle.activity.cachelist.presenter.CacheListAdapter;
 import com.google.code.geobeagle.database.DbFrontend;
 
+import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
@@ -32,26 +35,39 @@ public class MenuActionMyLocationTest {
 
     @Test
     public void testAct() {
-        CacheListAdapter cacheListAdapter = PowerMock.createMock(CacheListAdapter.class);
         Geocache geocache = PowerMock.createMock(Geocache.class);
         DbFrontend dbFrontend = PowerMock.createMock(DbFrontend.class);
-        //ErrorDisplayer errorDisplayer = PowerMock.createMock(ErrorDisplayer.class);
+        GeoFixProvider geoFixProvider = PowerMock.createMock(GeoFixProvider.class);
+        CacheAction cacheAction = PowerMock.createMock(CacheAction.class);
         GeocacheFactory geocacheFactory = PowerMock.createMock(GeocacheFactory.class);
-        cacheListAdapter.forceRefresh();
+        GeoFix geoFix = PowerMock.createMock(GeoFix.class);
+
+        EasyMock.expect(geoFix.getTime()).andReturn(1000L);
+        EasyMock.expect(geoFixProvider.getLocation()).andReturn(geoFix);
+        EasyMock.expect(geoFix.getLatitude()).andReturn(122.0);
+        EasyMock.expect(geoFix.getLongitude()).andReturn(-37.0);
+
+        EasyMock.expect(
+                geocacheFactory.create("ML160001", "[16:00] My Location", 122.0, -37.0,
+                        Source.MY_LOCATION, null, CacheType.MY_LOCATION, 0, 0, 0)).andReturn(
+                geocache);
         geocache.saveToDb(dbFrontend);
+        cacheAction.act(geocache);
 
         PowerMock.replayAll();
-        new MenuActionMyLocation(null, geocacheFactory, null, dbFrontend, null, null).act();
+        new MenuActionMyLocation(null, geocacheFactory, geoFixProvider, dbFrontend, null,
+                cacheAction).act();
         PowerMock.verifyAll();
     }
 
     @Test
     public void testActNullLocation() {
-        ErrorDisplayer errorDisplayer = PowerMock.createMock(ErrorDisplayer.class);
-        errorDisplayer.displayError(R.string.current_location_null);
+        GeoFixProvider geoFixProvider = PowerMock.createMock(GeoFixProvider.class);
 
+        EasyMock.expect(geoFixProvider.getLocation()).andReturn(null);
+        
         PowerMock.replayAll();
-        new MenuActionMyLocation(errorDisplayer, null, null, null, null, null).act();
+        new MenuActionMyLocation(null, null, geoFixProvider, null, null, null).act();
         PowerMock.verifyAll();
     }
 }

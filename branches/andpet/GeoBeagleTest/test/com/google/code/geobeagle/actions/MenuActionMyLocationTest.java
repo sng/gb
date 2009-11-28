9f712bc43b6af04ef04f85a79cee269fc063c10a
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-11-28 18:03:42 +0000

    fix tests
    git-svn-id: https://geobeagle.googlecode.com/svn@881 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/actions/MenuActionMyLocationTest.java
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/actions/MenuActionMyLocationTest.java
@@ -1,3 +1,16 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
 
 package com.google.code.geobeagle.actions;
 
@@ -9,6 +22,7 @@ import com.google.code.geobeagle.GeocacheFactory;
 import com.google.code.geobeagle.GeoFixProviderLive;
 import com.google.code.geobeagle.GeocacheFactory.Source;
 import com.google.code.geobeagle.activity.cachelist.actions.MenuActionMyLocation;
+import com.google.code.geobeagle.database.DbFrontend;
 
 import org.easymock.classextension.EasyMock;
 import org.junit.Test;
@@ -29,6 +43,9 @@ public class MenuActionMyLocationTest {
         GeoFix location = PowerMock.createMock(GeoFix.class);
         GeocacheFactory geocacheFactory = PowerMock.createMock(GeocacheFactory.class);
         Geocache geocache = PowerMock.createMock(Geocache.class);
+        DbFrontend dbFrontend = PowerMock.createMock(DbFrontend.class);
+        CacheAction cacheActionEdit = PowerMock.createMock(CacheAction.class);
+
         Locale.setDefault(Locale.ENGLISH);
         TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
 
@@ -40,9 +57,11 @@ public class MenuActionMyLocationTest {
                 geocacheFactory.create("ML161640", "[16:16] My Location", 37.0, -122.0,
                         Source.MY_LOCATION, null, CacheType.MY_LOCATION, 0, 0, 0)).andReturn(
                 geocache);
-
+        geocache.saveToDb(dbFrontend);
+        cacheActionEdit.act(geocache);
+        
         PowerMock.replayAll();
-        new MenuActionMyLocation(null, geocacheFactory, geoFixProvider, null, null, null).act();
+        new MenuActionMyLocation(null, geocacheFactory, geoFixProvider, dbFrontend, null, cacheActionEdit).act();
         PowerMock.verifyAll();
     }
 

135c44120d23bffdeaf77894ffc95c5fed04c846
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2009-12-21 15:51:04 +0000

    Fix tests.
    git-svn-id: https://geobeagle.googlecode.com/svn@932 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/database/CachesProviderDbTest.java
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/database/CachesProviderDbTest.java
@@ -20,6 +20,9 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import android.util.Log;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Log.class)
 public class CachesProviderDbTest {
@@ -39,12 +42,21 @@ public class CachesProviderDbTest {
 
     @Test
     public void testFilterUpdateSetsChanged() {
+        Set<Integer> requiredTags = new HashSet<Integer>();
+        requiredTags.add(1);
+        Set<Integer> forbiddenTags = new HashSet<Integer>();
+        forbiddenTags.add(2);
+        Set<Integer> updatedRequiredTags= new HashSet<Integer>();
+        updatedRequiredTags.add(22);
+        Set<Integer> updatedForbiddenTags = new HashSet<Integer>();
+        updatedForbiddenTags.add(0);
+
         expect(mCacheFilter1.getSqlWhereClause()).andReturn("1");
-        expect(mCacheFilter1.getRequiredTag()).andReturn(11);
-        expect(mCacheFilter1.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter1.getRequiredTags()).andReturn(requiredTags);
+        expect(mCacheFilter1.getForbiddenTags()).andReturn(forbiddenTags);
         expect(mCacheFilter2.getSqlWhereClause()).andReturn("2");
-        expect(mCacheFilter2.getRequiredTag()).andReturn(22);
-        expect(mCacheFilter2.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter2.getRequiredTags()).andReturn(updatedRequiredTags);
+        expect(mCacheFilter2.getForbiddenTags()).andReturn(updatedForbiddenTags);
 
         PowerMock.replayAll();
         mCachesProviderDb = new CachesProviderDb(mDbFrontend);
@@ -55,13 +67,17 @@ public class CachesProviderDbTest {
         PowerMock.verifyAll();
     }
 
+    @Test
     public void testUnchangedFilter() {
+        Set<Integer> tags = new HashSet<Integer>();
+        tags.add(0);
+        
         expect(mCacheFilter1.getSqlWhereClause()).andReturn("CacheType = 1").anyTimes();
-        expect(mCacheFilter1.getRequiredTag()).andReturn(0).anyTimes();
-        expect(mCacheFilter1.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter1.getRequiredTags()).andReturn(tags).anyTimes();
+        expect(mCacheFilter1.getForbiddenTags()).andReturn(tags);
         expect(mCacheFilter2.getSqlWhereClause()).andReturn("CacheType = 1").anyTimes();
-        expect(mCacheFilter2.getRequiredTag()).andReturn(0).anyTimes();
-        expect(mCacheFilter2.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter2.getRequiredTags()).andReturn(tags).anyTimes();
+        expect(mCacheFilter2.getForbiddenTags()).andReturn(tags);
         expect(mDbFrontend.countRaw((String)EasyMock.anyObject())).andReturn(5).anyTimes();
 
         PowerMock.replayAll();
@@ -74,16 +90,20 @@ public class CachesProviderDbTest {
         PowerMock.verifyAll();
     }
 
+    // Broken test.
     public void testExtraConditionGetNewCaches() {
+        Set<Integer> tags = new HashSet<Integer>();
+        tags.add(0);
+        
         GeocacheList list1 = new GeocacheListPrecomputed();
         expect(mDbFrontend.loadCachesRaw("SELECT Id FROM CACHES")).andReturn(list1);
         expect(mDbFrontend.loadCachesRaw("SELECT Id FROM CACHES WHERE CacheType = 1")).andReturn(list1);
         expect(mDbFrontend.countRaw((String)EasyMock.anyObject())).andReturn(0).anyTimes();
-        expect(mCacheFilter1.getRequiredTag()).andReturn(0).anyTimes();
-        expect(mCacheFilter1.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter1.getRequiredTags()).andReturn(tags).anyTimes();
+        expect(mCacheFilter1.getForbiddenTags()).andReturn(tags);
         expect(mCacheFilter1.getSqlWhereClause()).andReturn("");
-        expect(mCacheFilter2.getRequiredTag()).andReturn(0).anyTimes();
-        expect(mCacheFilter2.getForbiddenTag()).andReturn(0);
+        expect(mCacheFilter2.getRequiredTags()).andReturn(tags).anyTimes();
+        expect(mCacheFilter2.getForbiddenTags()).andReturn(tags);
         expect(mCacheFilter2.getSqlWhereClause()).andReturn("CacheType = 1");
 
         PowerMock.replayAll();

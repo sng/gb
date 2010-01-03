a69d560b677bbb8d89b647a8c08e8fca10aa3fe5
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-01-03 00:22:55 +0000

    increase test coverage
    git-svn-id: https://geobeagle.googlecode.com/svn@965 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/cachelist/CacheListAdapterTest.java
+++ branches/andpet/GeoBeagleTest/test/com/google/code/geobeagle/cachelist/CacheListAdapterTest.java
@@ -20,6 +20,8 @@ import com.google.code.geobeagle.Geocache;
 import com.google.code.geobeagle.GeocacheList;
 import com.google.code.geobeagle.activity.cachelist.presenter.CacheListAdapter;
 import com.google.code.geobeagle.activity.cachelist.presenter.GeocacheSummaryRowInflater;
+import com.google.code.geobeagle.activity.cachelist.presenter.CacheListAdapter.ScrollListener;
+import com.google.code.geobeagle.database.CachesProviderToggler;
 import com.google.code.geobeagle.database.DistanceAndBearing;
 import com.google.code.geobeagle.database.DistanceAndBearing.IDistanceAndBearingProvider;
 
@@ -31,6 +33,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -40,6 +43,68 @@ import android.widget.TextView;
 })
 public class CacheListAdapterTest {
 
+    @Test
+    public void testRefresh() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        GeocacheList geocacheList = PowerMock.createMock(GeocacheList.class);
+        CachesProviderToggler cachesProviderToggler = PowerMock.createMock(CachesProviderToggler.class);
+
+        EasyMock.expect(cachesProviderToggler.hasChanged()).andReturn(false);
+        
+        PowerMock.replayAll();
+        new CacheListAdapter(cachesProviderToggler, null, null, null,
+                geocacheList).refresh();
+        PowerMock.verifyAll();
+    }
+
+    @Test
+    public void testScrollListener() {
+        CacheListAdapter cacheListAdapter = PowerMock.createMock(CacheListAdapter.class);
+
+        cacheListAdapter.enableUpdates(true);
+        cacheListAdapter.enableUpdates(false);
+        
+        PowerMock.replayAll();
+        ScrollListener scrollListener = new ScrollListener(cacheListAdapter);
+        scrollListener.onScroll(null, 0, 0, 0);
+        scrollListener.onScrollStateChanged(null, 1);
+        scrollListener.onScrollStateChanged(null, AbsListView.OnScrollListener.SCROLL_STATE_IDLE);
+        PowerMock.verifyAll();
+    }
+    
+    @Test
+    public void testGetGeocacheAtNullListData() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        
+        PowerMock.replayAll();
+        new CacheListAdapter(null, null, null, null, null).getGeocacheAt(0);
+        PowerMock.verifyAll();
+    }
+
+    @Test
+    public void testSetAzimuth() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        
+        PowerMock.replayAll();
+        new CacheListAdapter(null, null, null, null, null).setAzimuth(12f);
+        PowerMock.verifyAll();
+    }
+
+    @Test
+    public void testGetGeocacheAt() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        GeocacheList geocacheList = PowerMock.createMock(GeocacheList.class);
+        Geocache geocache = PowerMock.createMock(Geocache.class);
+        
+        EasyMock.expect(geocacheList.get(12)).andReturn(geocache);
+        
+        PowerMock.replayAll();
+        assertEquals(geocache, new CacheListAdapter(null, null, null, null,
+                geocacheList).getGeocacheAt(12));
+        PowerMock.verifyAll();
+    }
+
+    
     @Test
     public void testGetCount() {
         PowerMock.suppressConstructor(BaseAdapter.class);
@@ -52,6 +117,16 @@ public class CacheListAdapterTest {
         PowerMock.verifyAll();
     }
 
+    @Test
+    public void testGetCountNull() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        
+        PowerMock.replayAll();
+        assertEquals(0, new CacheListAdapter(null, null, null, null, null).getCount());
+        PowerMock.verifyAll();
+    }
+
+    
     @Test
     public void testGetItem() {
         PowerMock.suppressConstructor(BaseAdapter.class);
@@ -70,6 +145,18 @@ public class CacheListAdapterTest {
         PowerMock.verifyAll();
     }
 
+
+    @Test
+    public void testGetViewNull() {
+        PowerMock.suppressConstructor(BaseAdapter.class);
+        View convertView = PowerMock.createMock(View.class);
+
+        PowerMock.replayAll();
+        assertEquals(null, new CacheListAdapter(null, null, null, null, null)
+                .getView(17, convertView, null));
+        PowerMock.verifyAll();
+    }
+    
     @Test
     public void testGetView() {
         GeocacheSummaryRowInflater geocacheSummaryRowInflater = PowerMock

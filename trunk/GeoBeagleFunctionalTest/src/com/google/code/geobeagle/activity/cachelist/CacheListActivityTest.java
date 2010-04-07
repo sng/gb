1b85e4022da405abc658bc02bb6c5c8e02123cc0
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-04-07 06:28:25 +0000

    update functional tests
    git-svn-id: https://geobeagle.googlecode.com/svn@1037 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagleFunctionalTest/src/com/google/code/geobeagle/activity/cachelist/CacheListActivityTest.java
+++ trunk/GeoBeagleFunctionalTest/src/com/google/code/geobeagle/activity/cachelist/CacheListActivityTest.java
@@ -14,13 +14,21 @@
 
 package com.google.code.geobeagle.activity.cachelist;
 
-import com.google.code.geobeagle.activity.MenuActionStub;
-import com.google.code.geobeagle.activity.cachelist.CacheListActivity;
+import com.google.code.geobeagle.R;
 
+import org.easymock.EasyMock;
+
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 
 public class CacheListActivityTest extends
         ActivityInstrumentationTestCase2<CacheListActivity> {
+
+    private CacheListActivity cacheListActivity;
+    private Instrumentation instrumentation;
+    private MenuItem item;
 
     public CacheListActivityTest() {
         super("com.google.code.geobeagle", CacheListActivity.class);
@@ -29,19 +37,68 @@ public class CacheListActivityTest extends
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        instrumentation = getInstrumentation();
+        item = EasyMock.createMock(MenuItem.class);
+        cacheListActivity = getActivity();
     }
 
-    public void testCreate() {
-        final CacheListActivity cacheListActivity = getActivity();
-        assertNotNull(cacheListActivity.locationControlBuffered);
-        final MenuActionStub menuActionStub = new MenuActionStub();
-        getInstrumentation().addMonitor(
-                cacheListActivity.getClass().getCanonicalName(), null, false);
-        getInstrumentation().runOnMainSync(new Runnable() {
-            public void run() {
-                cacheListActivity.onOptionsItemSelected(menuActionStub);
-            }
-        });
-        getInstrumentation().waitForIdleSync();
+    public void testAddMyLocation() throws InterruptedException {
+        EasyMock.expect(item.getItemId()).andReturn(
+                R.string.menu_add_my_location);
+
+        EasyMock.replay(item);
+        cacheListActivity.onOptionsItemSelected(item);
+        EasyMock.verify(item);
+
+        instrumentation.waitForIdleSync();
+        Thread.sleep(2000);
+
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+    }
+
+    public void testSearchOnline() throws InterruptedException {
+        EasyMock.expect(item.getItemId())
+                .andReturn(R.string.menu_search_online);
+
+        EasyMock.replay(item);
+        cacheListActivity.onOptionsItemSelected(item);
+        EasyMock.verify(item);
+
+        instrumentation.waitForIdleSync();
+        Thread.sleep(2000);
+
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+    }
+
+    public void testShowMap() throws InterruptedException {
+        EasyMock.expect(item.getItemId()).andReturn(R.string.menu_map);
+
+        EasyMock.replay(item);
+        cacheListActivity.onOptionsItemSelected(item);
+        EasyMock.verify(item);
+
+        instrumentation.waitForIdleSync();
+        Thread.sleep(2000);
+
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+    }
+
+    public void testShowAllCaches() throws InterruptedException {
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_LEFT);
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_ENTER);
+        Thread.sleep(2000);
+    }
+
+    public void testSync() {
+        EasyMock.expect(item.getItemId()).andReturn(R.string.menu_sync);
+
+        EasyMock.replay(item);
+        cacheListActivity.onOptionsItemSelected(item);
+        EasyMock.verify(item);
+        instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+        instrumentation.waitForIdleSync();
     }
 }

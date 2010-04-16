c8c7f499ab24ed1b3879a5a8cca736f58f7c39e2
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-04-16 07:45:03 +0000

    Refactor code for drawing attributes (difficulty and terrain): clarify method names, remove commented-out code, extract common drawAttribute() code.
    git-svn-id: https://geobeagle.googlecode.com/svn@1066 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- branches/andpet/GeoBeagle/di/com/google/code/geobeagle/GraphicsGenerator.java
+++ branches/andpet/GeoBeagle/di/com/google/code/geobeagle/GraphicsGenerator.java
@@ -4,7 +4,6 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
@@ -57,19 +56,19 @@ public class GraphicsGenerator {
         return ratings;
     }
 
-    private Drawable createOverlay(Geocache geocache, int thickness, int bottom, 
+    Drawable createOverlay(Geocache geocache, int thickness, int bottom, 
             int backdropId, Drawable overlayIcon, Resources resources) {
         Bitmap bitmap = BitmapFactory.decodeResource(resources, backdropId);
         int imageHeight = bitmap.getHeight();
         int imageWidth = bitmap.getWidth();
-        
+
         Bitmap copy;
         if (bottom >= 0) {
             copy = bitmap.copy(Bitmap.Config.ARGB_8888, true);
         } else {
-            copy = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight()-bottom, 
+            copy = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight() - bottom,
                     Bitmap.Config.ARGB_8888);
-            int[] pixels = new int[imageWidth*imageHeight];
+            int[] pixels = new int[imageWidth * imageHeight];
             bitmap.getPixels(pixels, 0, imageWidth, 0, 0, imageWidth, imageHeight);
             copy.setPixels(pixels, 0, imageWidth, 0, 0, imageWidth, imageHeight);
             imageHeight = copy.getHeight();
@@ -78,43 +77,43 @@ public class GraphicsGenerator {
 
         Canvas canvas = new Canvas(copy);
 
-        //mTempPaint.setColor(Color.BLUE);
-        //mTempPaint.setARGB(255, 0x20, 0x20, 0xFF);  //light blue
-        mTempPaint.setColor(Color.YELLOW);
-        int diffWidth = (int)(imageWidth * (geocache.getDifficulty()/10.0));
-        mTempRect.set(0, imageHeight-bottom-2*thickness-2, 
-                diffWidth, imageHeight-bottom-thickness-2);
-        canvas.drawRect(mTempRect, mTempPaint);
+        mTempPaint.setARGB(255, 0x20, 0x20, 0xFF); // light blue
+        drawAttribute(1, thickness, bottom, imageHeight, imageWidth, canvas, geocache.getDifficulty());
+
+        mTempPaint.setARGB(255, 0xDB, 0xA1, 0x09); // a lighter brown
+        drawAttribute(0, thickness, bottom, imageHeight, imageWidth, canvas, geocache.getTerrain());
         
-        mTempPaint.setARGB(255, 0xDB, 0xA1, 0x09);  //a lighter brown
-        //mTempPaint.setARGB(255, 139, 94, 23);  //same color as paws
-        int terrWidth = (int)(imageWidth * (geocache.getTerrain()/10.0));
-        //Draw on right side:
-        //mTempRect.set(imageWidth-terrWidth, imageHeight-bottom-thickness-1,
-        //        imageWidth-1, imageHeight-bottom-1);
-        //Draw on left side:
-        mTempRect.set(0, imageHeight-bottom-thickness-1,
-                terrWidth, imageHeight-bottom-1);
+        drawOverlay(overlayIcon, imageWidth, canvas);
+            
+        return new BitmapDrawable(copy);
+    }
+
+    private void drawAttribute(int position, int thickness, int bottom, int imageHeight,
+            int imageWidth, Canvas canvas, double attribute) {
+        final int diffWidth = (int)(imageWidth * (attribute / 10.0));
+        final int MARGIN = 1;
+        final int base = imageHeight - bottom - MARGIN;
+        final int attributeBottom = base - position * (thickness + 1);
+        final int attributeTop = attributeBottom - thickness;
+        mTempRect.set(0, attributeTop, diffWidth, attributeBottom);
         canvas.drawRect(mTempRect, mTempPaint);
-        
+    }
+
+    private void drawOverlay(Drawable overlayIcon, int imageWidth, Canvas canvas) {
         if (overlayIcon != null) {
             overlayIcon.setBounds(imageWidth-1-overlayIcon.getIntrinsicWidth(),
                     0, imageWidth-1, overlayIcon.getIntrinsicHeight()-1);
             overlayIcon.draw(canvas);
         }
-            
-        return new BitmapDrawable(copy);
     }
     
-    /** Creates the icon to use in the list view */
-    public Drawable createIcon(Geocache geocache, Drawable overlayIcon, 
+    public Drawable createIconListView(Geocache geocache, Drawable overlayIcon, 
             Resources resources) {
         return createOverlay(geocache, 3, -5, geocache.getCacheType().icon(), 
                 overlayIcon, resources);
     }
     
-    /** Creates the icon to use in the map view */
-    public Drawable createIconMap(Geocache geocache, Drawable overlayIcon, 
+    public Drawable createIconMapView(Geocache geocache, Drawable overlayIcon, 
             Resources resources) {
         Drawable iconMap = createOverlay(geocache, 3, 3, 
                 geocache.getCacheType().iconMap(), overlayIcon, resources);

021ef887a79285d691b59bf6b80b4b0095acae72
Stephen Ng <stephen@theleengs.com>
2010-05-10 20:08:52 -0700

    Refactor bcaching loop.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/bcaching/ImportBCachingWorker.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/bcaching/ImportBCachingWorker.java
@@ -15,12 +15,14 @@
 package com.google.code.geobeagle.bcaching;
 
 import com.google.code.geobeagle.cachedetails.WriterWrapper;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
 
+import org.apache.http.HttpException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -32,21 +34,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.nio.charset.Charset;
+import java.security.DigestException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Hashtable;
 
 public class ImportBCachingWorker extends Thread {
-
-    private final Handler handler;
-    private final SharedPreferences sharedPreferences;
-
-    public ImportBCachingWorker(Handler handler, SharedPreferences sharedPreferences) {
-        this.handler = handler;
-        this.sharedPreferences = sharedPreferences;
+    public static interface ImportBCachingWorkerFactory {
+        ImportBCachingWorker create(Handler handler);
     }
 
-    private JSONObject readResponse(InputStream in) throws IOException, JSONException {
-        BufferedReader rd = new BufferedReader(new InputStreamReader(in, Charset
-                .forName("UTF-8")), 8192);
+    private final Handler handler;
+    private final BCachingLastUpdated bcachingLastUpdated;
+    private final BCachingListFactory bCachingListFactory;
+    private final BCachingCommunication bCachingCommunication;
+
+    @Inject
+    public ImportBCachingWorker(@Assisted Handler handler, BCachingLastUpdated bcachingLastUpdated,
+            BCachingListFactory bCachingListFactory, BCachingCommunication bCachingCommunication) {
+        this.handler = handler;
+        this.bcachingLastUpdated = bcachingLastUpdated;
+        this.bCachingListFactory = bCachingListFactory;
+        this.bCachingCommunication = bCachingCommunication;
+    }
+
+    static JSONObject readResponse(InputStream in) throws IOException, JSONException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8")),
+                8192);
 
         StringBuilder result = new StringBuilder();
         String line;
@@ -73,75 +86,116 @@ public class ImportBCachingWorker extends Thread {
         params.put("app", "GeoBeagle");
 
         Log.d("GeoBeagle", "Downloading cache details");
-        InputStream is = bcachingCommunication.sendRequest(params);
-        DataInputStream dis = new DataInputStream(is);
-        String line = null;
+        DataInputStream dis = new DataInputStream(bcachingCommunication.sendRequest(params));
         WriterWrapper writerWrapper = new WriterWrapper();
-        writerWrapper
-                .open("/sdcard/download/bcaching" + String.valueOf(updatedCaches) + ".gpx");
+        writerWrapper.open("/sdcard/download/bcaching" + String.valueOf(updatedCaches) + ".gpx");
+        String line;
         while ((line = dis.readLine()) != null) {
             writerWrapper.write(line);
         }
         writerWrapper.close();
     }
 
-    @Override
-    public void run() {
-        String now = Long.toString(System.currentTimeMillis());
-        try {
-            String lastUpdate = sharedPreferences.getString("bcaching_lastupdate", "");
-            BCachingCommunication bcachingCommunication = new BCachingCommunication("USERNAME",
-                    "PASSWORD");
-            Message.obtain(handler, ProgressMessage.START.ordinal()).sendToTarget();
+    static class BCachingList {
+        private final JSONObject cacheList;
 
-            Hashtable<String, String> params = new Hashtable<String, String>();
+        BCachingList(JSONObject json) {
+            this.cacheList = json;
+        }
+
+        int getCount() throws JSONException {
+            return cacheList.getJSONArray("data").length();
+        }
+
+        int getTotalCount() throws JSONException {
+            return cacheList.getInt("totalCount");
+        }
+
+        String getCacheIds() throws JSONException {
+            JSONArray summary = cacheList.getJSONArray("data");
+            Log.d("GeoBeagle", summary.toString());
+
+            StringBuilder csvIds = new StringBuilder();
+            int count = summary.length();
+            for (int i = 0; i < count; i++) {
+                JSONObject cacheObject = summary.getJSONObject(i);
+                int id = cacheObject.getInt("id");
+                if (csvIds.length() > 0) {
+                    csvIds.append(',');
+                }
+                csvIds.append(String.valueOf(id));
+            }
+            return csvIds.toString();
+        }
+    }
+
+    static class BCachingListFactory {
+        private final Hashtable<String, String> params;
+        private final BCachingCommunication bCachingCommunication;
+        private BCachingList firstCacheList;
+
+        @Inject
+        BCachingListFactory(Hashtable<String, String> params,
+                BCachingCommunication bCachingCommunication) {
+            this.params = params;
+            this.bCachingCommunication = bCachingCommunication;
+        }
+
+        int getTotalCount() throws MalformedURLException, IOException, JSONException,
+                NoSuchAlgorithmException, DigestException, HttpException {
+            params.remove("first");
+            firstCacheList = new BCachingList(readResponse(bCachingCommunication
+                    .sendRequest(params)));
+            return firstCacheList.getTotalCount();
+        }
+
+        void putLastUpdateTime(String lastUpdate) {
             if (!lastUpdate.equals(""))
                 params.put("since", lastUpdate);
-            params.put("a", "list");
-            params.put("maxcount", "50");
-            params.put("found", "0");
-            params.put("app", "GeoBeagle");
+        }
 
-            int updatedCaches = 0;
+        BCachingList getCacheList(int startAt) throws NoSuchAlgorithmException, DigestException,
+                IOException, JSONException, HttpException {
+            if (startAt == 0)
+                return firstCacheList;
 
-            JSONObject obj = readResponse(bcachingCommunication.sendRequest(params));
-            int totalCount = obj.getInt("totalCount");
+            params.put("first", Integer.toString(startAt));
+            return new BCachingList(readResponse(bCachingCommunication.sendRequest(params)));
+        }
+
+    }
+
+    @Override
+    public void run() {
+        long now = System.currentTimeMillis();
+        try {
+            Message.obtain(handler, ProgressMessage.START.ordinal()).sendToTarget();
+            bCachingListFactory.putLastUpdateTime(bcachingLastUpdated.getLastUpdateTime());
+
+            int totalCount = bCachingListFactory.getTotalCount();
             Message.obtain(handler, ProgressMessage.SET_MAX.ordinal(), totalCount, 0)
                     .sendToTarget();
             Log.d("GeoBeagle", "totalCount = " + totalCount);
 
+            int updatedCaches = 0;
             while (updatedCaches < totalCount) {
-                params.put("first", Integer.toString(updatedCaches));
+                BCachingList bcachingList = bCachingListFactory.getCacheList(updatedCaches);
 
-                if (updatedCaches > 0)
-                    obj = readResponse(bcachingCommunication.sendRequest(params));
-
-                JSONArray summary = obj.getJSONArray("data");
-                Log.d("GeoBeagle", summary.toString());
-
-                int count = summary.length();
-                StringBuilder csvIds = new StringBuilder();
-                for (int i = 0; i < count; i++) {
-                    JSONObject cacheObject = summary.getJSONObject(i);
-                    int id = cacheObject.getInt("id");
-                    if (csvIds.length() > 0) {
-                        csvIds.append(',');
-                    }
-                    csvIds.append(String.valueOf(id));
-                }
-
-                updatedCaches += count;
+                updatedCaches += bcachingList.getCount();
+                Log.d("GeoBeagle", "updated caches: " + updatedCaches);
                 Message.obtain(handler, ProgressMessage.SET_PROGRESS.ordinal(), updatedCaches, 0)
                         .sendToTarget();
-                getCacheDetails(bcachingCommunication, csvIds.toString(), updatedCaches);
+                getCacheDetails(bCachingCommunication, bcachingList.getCacheIds(), updatedCaches);
+                if (updatedCaches < 50)
+                    break;
             }
         } catch (Exception ex) {
-
+            Log.d("GeoBeagle", "Exception: " + ex);
         }
+
         Message.obtain(handler, ProgressMessage.DONE.ordinal(), 0).sendToTarget();
         Log.d("GeoBeagle", "Setting bcaching_lastupdate to " + now);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("bcaching_lastupdate", now);
-        editor.commit();
+        bcachingLastUpdated.putLastUpdateTime(now);
     }
+
 }

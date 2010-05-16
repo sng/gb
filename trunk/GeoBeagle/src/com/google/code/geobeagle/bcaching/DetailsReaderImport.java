3b7a4da0670bf42e7aee9b38bd8ba2bf12dcebe0
Stephen Ng <stephen@theleengs.com>
2010-05-15 22:26:16 -0700

    remove params

--- trunk/GeoBeagle/src/com/google/code/geobeagle/bcaching/DetailsReaderImport.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/bcaching/DetailsReaderImport.java
@@ -1,23 +1,15 @@
 
 package com.google.code.geobeagle.bcaching;
 
-import com.google.code.geobeagle.ErrorDisplayer;
 import com.google.code.geobeagle.bcaching.BCachingAnnotations.DetailsReaderAnnotation;
 import com.google.code.geobeagle.bcaching.communication.BCachingException;
 import com.google.code.geobeagle.bcaching.communication.BCachingListImportHelper.BufferedReaderFactory;
-import com.google.code.geobeagle.xmlimport.CachePersisterFacade;
 import com.google.code.geobeagle.xmlimport.EventHelper;
-import com.google.code.geobeagle.xmlimport.FileAlreadyLoadedChecker;
 import com.google.code.geobeagle.xmlimport.GpxLoader;
-import com.google.code.geobeagle.xmlimport.GpxToCache;
-import com.google.code.geobeagle.xmlimport.GpxToCache.Aborter;
-import com.google.code.geobeagle.xmlimport.GpxToCacheDI.XmlPullParserWrapper;
 import com.google.code.geobeagle.xmlimport.XmlimportAnnotations.GpxAnnotation;
 import com.google.inject.Inject;
 
 import org.xmlpull.v1.XmlPullParserException;
-
-import android.os.PowerManager.WakeLock;
 
 import java.io.BufferedReader;
 import java.util.Hashtable;
@@ -26,16 +18,14 @@ public class DetailsReaderImport {
     private final Hashtable<String, String> params;
     private final BufferedReaderFactory bufferedReaderFactory;
     private final GpxLoader gpxLoader;
-    private EventHelper eventHelper;
+    private final EventHelper eventHelper;
 
     @Inject
     DetailsReaderImport(@DetailsReaderAnnotation Hashtable<String, String> params,
-            BufferedReaderFactory bufferedReaderFactory, ErrorDisplayer errorDisplayer,
-            WakeLock wakeLock, CachePersisterFacade cachePersisterFacade,
-            @GpxAnnotation EventHelper eventHelper, GpxToCache gpxToCache) {
+            BufferedReaderFactory bufferedReaderFactory, @GpxAnnotation EventHelper eventHelper, GpxLoader gpxLoader) {
         this.params = params;
         this.bufferedReaderFactory = bufferedReaderFactory;
-        this.gpxLoader = new GpxLoader(cachePersisterFacade, errorDisplayer, gpxToCache, wakeLock);
+        this.gpxLoader = gpxLoader;
         this.eventHelper = eventHelper;
     }
 

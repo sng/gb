7bd0ae946e5188cb3eb37450e8bbc055ca58b446
Stephen Ng <stephen@theleengs.com>
2010-05-19 06:20:43 -0700

    add roboguice task helpers.

---
+++ trunk/GeoBeagle/di/roboguice/util/RoboAsyncTask.java
@@ -0,0 +1,60 @@
+package roboguice.util;
+
+import roboguice.inject.ContextScope;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Allows injection to happen for tasks that execute in a background thread.
+ * 
+ * @param <ResultT>
+ */
+public abstract class RoboAsyncTask<ResultT> extends SafeAsyncTask<ResultT> {
+    @Inject static protected Provider<Context> contextProvider;
+    @Inject static protected Provider<ContextScope> scopeProvider;
+    
+    protected ContextScope scope = scopeProvider.get();
+    protected Context context = contextProvider.get();
+
+    protected RoboAsyncTask() {
+    }
+
+    protected RoboAsyncTask(Handler handler) {
+        super(handler);
+    }
+
+    protected RoboAsyncTask(Handler handler, ThreadFactory threadFactory) {
+        super(handler, threadFactory);
+    }
+
+    protected RoboAsyncTask(ThreadFactory threadFactory) {
+        super(threadFactory);
+    }
+
+    @Override
+    protected Task<ResultT> newTask() {
+        return new RoboTask<ResultT>(this);
+    }
+
+    protected class RoboTask<ResultT> extends SafeAsyncTask.Task<ResultT> {
+        public RoboTask(SafeAsyncTask parent) {
+            super(parent);
+        }
+
+        @Override
+        protected ResultT doCall() throws Exception {
+            try {
+                scope.enter(context);
+                return super.doCall();
+            } finally {
+                scope.exit(context);
+            }
+        }
+    }
+}

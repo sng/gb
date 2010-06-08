b4c9f972e37df27fe64f22889039cdebbe1e73b5
Stephen Ng <stephen@theleengs.com>
2010-06-08 08:52:31 -0700

    Inline create.

--- trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
@@ -2,10 +2,14 @@
 package com.google.code.geobeagle.gpsstatuswidget;
 
 import com.google.code.geobeagle.LocationControlBuffered;
+import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.Time;
 import com.google.code.geobeagle.activity.cachelist.ActivityVisible;
 import com.google.code.geobeagle.formatting.DistanceFormatter;
 import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule.GpsStatusWidgetView;
+import com.google.code.geobeagle.gpsstatuswidget.TextLagUpdater.LagNull;
+import com.google.code.geobeagle.gpsstatuswidget.TextLagUpdater.LastKnownLocationUnavailable;
+import com.google.code.geobeagle.gpsstatuswidget.TextLagUpdater.LastLocationUnknown;
 import com.google.code.geobeagle.location.CombinedLocationManager;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -13,6 +17,7 @@ import com.google.inject.Provider;
 import android.content.Context;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
 public class GpsWidgetAndUpdater {
     private final GpsStatusWidgetDelegate mGpsStatusWidgetDelegate;
@@ -24,8 +29,13 @@ public class GpsWidgetAndUpdater {
             CombinedLocationManager combinedLocationManager,
             Provider<DistanceFormatter> distanceFormatterProvider, ActivityVisible activityVisible,
             Time time, Handler handler, MeterBars meterBars, Meter meter) {
-        final TextLagUpdater textLagUpdater = GpsStatusWidget.createTextLagUpdater(gpsWidgetView,
-                combinedLocationManager, time);
+        final TextView lag = (TextView)gpsWidgetView.findViewById(R.id.lag);
+        final LagNull lagNull = new LagNull();
+        final LastKnownLocationUnavailable lastKnownLocationUnavailable = new LastKnownLocationUnavailable(
+                lagNull);
+        final LastLocationUnknown lastLocationUnknown = new LastLocationUnknown(
+                combinedLocationManager, lastKnownLocationUnavailable);
+        final TextLagUpdater textLagUpdater = new TextLagUpdater(lastLocationUnknown, lag, time);
         mUpdateGpsRunnable = new UpdateGpsWidgetRunnable(handler, mLocationControlBuffered, meter,
                 textLagUpdater, activityVisible);
         mGpsStatusWidgetDelegate = GpsStatusWidget.createGpsStatusWidgetDelegate(gpsWidgetView,

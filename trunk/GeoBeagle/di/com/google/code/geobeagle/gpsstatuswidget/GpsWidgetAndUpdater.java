908b1ea397728f7cfcf9deee27e0dcc342272378
Stephen Ng <stephen@theleengs.com>
2010-06-08 20:15:52 -0700

    Inject createGpsStatusWidgetDelegate.

--- trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
@@ -1,6 +1,7 @@
 
 package com.google.code.geobeagle.gpsstatuswidget;
 
+import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.Time;
 import com.google.code.geobeagle.formatting.DistanceFormatter;
 import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule.GpsStatusWidgetView;
@@ -10,6 +11,7 @@ import com.google.inject.Provider;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 
 public class GpsWidgetAndUpdater {
     private final GpsStatusWidgetDelegate mGpsStatusWidgetDelegate;
@@ -21,9 +23,11 @@ public class GpsWidgetAndUpdater {
             Provider<DistanceFormatter> distanceFormatterProvider, Time time, MeterBars meterBars,
             Meter meter, TextLagUpdater textLagUpdater, UpdateGpsWidgetRunnable updateGpsRunnable) {
         mUpdateGpsRunnable = updateGpsRunnable;
-        mGpsStatusWidgetDelegate = GpsStatusWidget.createGpsStatusWidgetDelegate(gpsWidgetView,
-                time, combinedLocationManager, meter, distanceFormatterProvider, meterBars,
-                textLagUpdater, context);
+        final TextView status = (TextView)gpsWidgetView.findViewById(R.id.status);
+        final TextView provider = (TextView)gpsWidgetView.findViewById(R.id.provider);
+        final MeterFader meterFader = new MeterFader(gpsWidgetView, meterBars, time);
+        mGpsStatusWidgetDelegate = new GpsStatusWidgetDelegate(combinedLocationManager, distanceFormatterProvider,
+        meter, meterFader, provider, context, status, textLagUpdater);
     }
 
     public GpsStatusWidgetDelegate getGpsStatusWidgetDelegate() {

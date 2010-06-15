10e0471d96ca6c604e36c0f0896ebde6c1a20b83
Stephen Ng <stephen@theleengs.com>
2010-06-15 07:15:23 -0700

    Minor cleanup.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/XmlPullParserWrapper.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/XmlPullParserWrapper.java
@@ -18,17 +18,12 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import android.util.Log;
-
 import java.io.IOException;
 import java.io.Reader;
 
 public class XmlPullParserWrapper {
     private String mSource;
     private XmlPullParser mXmlPullParser;
-
-    public XmlPullParserWrapper() {
-    }
 
     public String getAttributeValue(String namespace, String name) {
         return mXmlPullParser.getAttributeValue(namespace, name);
@@ -55,7 +50,6 @@ public class XmlPullParserWrapper {
     }
 
     public void open(String path, Reader reader) throws XmlPullParserException {
-        Log.d("GeoBeagle", "XmlPullParserWrapper open: " + this + ", " + path + ", " + reader);
         XmlPullParser newPullParser = XmlPullParserFactory.newInstance().newPullParser();
         newPullParser.setInput(reader);
         mSource = path;

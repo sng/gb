bf8879e21edae6f378ccbfe08431e6f3f53fcc1e
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-06-21 13:59:01 +0000

    Replace GpsWidgetAndUpdater factory with injection from private modules.
    git-svn-id: https://geobeagle.googlecode.com/svn@1373 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
@@ -6,10 +6,10 @@ import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.Time;
 import com.google.code.geobeagle.activity.cachelist.ActivityVisible;
 import com.google.code.geobeagle.formatting.DistanceFormatter;
+import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule.GpsStatusWidgetView;
 import com.google.code.geobeagle.location.CombinedLocationManager;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.google.inject.assistedinject.Assisted;
 
 import android.content.Context;
 import android.os.Handler;
@@ -20,18 +20,14 @@ public class GpsWidgetAndUpdater {
     private final GpsStatusWidgetDelegate mGpsStatusWidgetDelegate;
     private final UpdateGpsWidgetRunnable mUpdateGpsRunnable;
 
-    public interface GpsWidgetAndUpdaterFactory {
-        public GpsWidgetAndUpdater create(View gpsWidgetView);
-    }
-
     @Inject
-    public GpsWidgetAndUpdater(Context context, @Assisted View gpsWidgetView,
+    public GpsWidgetAndUpdater(Context context, @GpsStatusWidgetView View gpsWidgetView,
             LocationControlBuffered mLocationControlBuffered,
             CombinedLocationManager combinedLocationManager,
             Provider<DistanceFormatter> distanceFormatterProvider, ActivityVisible activityVisible,
             Time time, Handler handler) {
         final MeterFormatter meterFormatter = new MeterFormatter(context);
-                final TextView locationViewer = (TextView)gpsWidgetView.findViewById(R.id.location_viewer);
+        final TextView locationViewer = (TextView)gpsWidgetView.findViewById(R.id.location_viewer);
         final MeterBars meterBars = new MeterBars(locationViewer, meterFormatter);
         final TextView accuracyView = (TextView)gpsWidgetView.findViewById(R.id.accuracy);
         final Meter meter = new Meter(meterBars, accuracyView);

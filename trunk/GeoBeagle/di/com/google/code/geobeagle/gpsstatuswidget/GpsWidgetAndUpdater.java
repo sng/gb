ce6253d81b96d3bd191c535a90266759a7627321
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-06-21 13:58:48 +0000

    Inline MeterBars create.
    git-svn-id: https://geobeagle.googlecode.com/svn@1371 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/gpsstatuswidget/GpsWidgetAndUpdater.java
@@ -2,6 +2,7 @@
 package com.google.code.geobeagle.gpsstatuswidget;
 
 import com.google.code.geobeagle.LocationControlBuffered;
+import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.Time;
 import com.google.code.geobeagle.activity.cachelist.ActivityVisible;
 import com.google.code.geobeagle.formatting.DistanceFormatter;
@@ -13,6 +14,7 @@ import com.google.inject.assistedinject.Assisted;
 import android.content.Context;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
 public class GpsWidgetAndUpdater {
     private final GpsStatusWidgetDelegate mGpsStatusWidgetDelegate;
@@ -28,7 +30,9 @@ public class GpsWidgetAndUpdater {
             CombinedLocationManager combinedLocationManager,
             Provider<DistanceFormatter> distanceFormatterProvider, ActivityVisible activityVisible,
             Time time, Handler handler) {
-        final MeterBars meterBars = GpsStatusWidget.create(context, gpsWidgetView);
+        final MeterFormatter meterFormatter = new MeterFormatter(context);
+                final TextView locationViewer = (TextView)gpsWidgetView.findViewById(R.id.location_viewer);
+        final MeterBars meterBars = new MeterBars(locationViewer, meterFormatter);
         final Meter meter = GpsStatusWidget.createMeterWrapper(gpsWidgetView, meterBars);
         final TextLagUpdater textLagUpdater = GpsStatusWidget.createTextLagUpdater(gpsWidgetView,
                 combinedLocationManager, time);

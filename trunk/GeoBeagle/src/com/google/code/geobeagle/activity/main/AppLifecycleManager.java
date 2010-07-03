4968ff916e7b6c72613b1e2154b306288cc40ffa
Stephen Ng <stephen@theleengs.com>
2010-07-02 19:44:17 -0700

    Unprovide AppLifecycleManager.
    .

--- trunk/GeoBeagle/src/com/google/code/geobeagle/activity/main/AppLifecycleManager.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/main/AppLifecycleManager.java
@@ -14,14 +14,27 @@
 
 package com.google.code.geobeagle.activity.main;
 
+import com.google.code.geobeagle.LocationControlBuffered;
+import com.google.code.geobeagle.GeoBeaglePackageModule.DefaultSharedPreferences;
+import com.google.code.geobeagle.location.LocationLifecycleManager;
+import com.google.inject.Inject;
+
 import android.content.SharedPreferences;
+import android.location.LocationManager;
 
 public class AppLifecycleManager {
     private final LifecycleManager[] mLifecycleManagers;
     private final SharedPreferences mPreferences;
 
-    public AppLifecycleManager(SharedPreferences preferences, LifecycleManager[] lifecycleManagers) {
-        mLifecycleManagers = lifecycleManagers;
+    @Inject
+    public AppLifecycleManager(@DefaultSharedPreferences SharedPreferences preferences,
+            LocationControlBuffered locationControlBuffered, LocationManager locationManager,
+            RadarView radarView) {
+        mLifecycleManagers = new LifecycleManager[] {
+                new LocationLifecycleManager(locationControlBuffered, locationManager),
+                new LocationLifecycleManager(radarView, locationManager)
+        };
+
         mPreferences = preferences;
     }
 

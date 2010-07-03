d5fffbdbbf736c075987afe5d7899f993b371354
Stephen Ng <stephen@theleengs.com>
2010-07-02 17:37:19 -0700

    Unprovide GeocacheDifficulty.

--- trunk/GeoBeagle/di/com/google/code/geobeagle/activity/main/GeoBeagleModule.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/activity/main/GeoBeagleModule.java
@@ -162,24 +162,6 @@ public class GeoBeagleModule extends AbstractAndroidModule {
     }
 
     @Provides
-    @Named("GeocacheDifficulty")
-    AttributeViewer providesGeocacheDifficulty(Activity activity, Resources resources,
-            RatingsArray ratingsArray) {
-        final Drawable[] ribbonDrawables = {
-                resources.getDrawable(R.drawable.ribbon_unselected_dark),
-                resources.getDrawable(R.drawable.ribbon_half_bright),
-                resources.getDrawable(R.drawable.ribbon_selected_bright)
-        };
-        final ImageView imageView = (ImageView)activity.findViewById(R.id.gc_difficulty);
-        AttributeViewer ribbonImages =
-
-        getImagesOnDifficulty(ribbonDrawables, imageView, ratingsArray);
-
-        return new LabelledAttributeViewer(
-                (TextView)activity.findViewById(R.id.gc_text_difficulty), ribbonImages);
-    }
-
-    @Provides
     @Named("GeocacheTerrain")
     AttributeViewer providesGeocacheTerrain(Activity activity, Resources resources,
             RatingsArray ratingsArray) {
@@ -257,17 +239,28 @@ public class GeoBeagleModule extends AbstractAndroidModule {
                 .asList(GeocacheViewer.CONTAINER_IMAGES));
     }
 
-
     @Provides
     public GeocacheViewer providesGeocacheViewer(RadarView radarView, Activity activity,
-            @Named("GeocacheDifficulty") AttributeViewer gcDifficulty,
             @Named("GeocacheTerrain") AttributeViewer gcTerrain, ResourceImages gcContainer,
             IconOverlayFactory iconOverlayFactory, MapViewBitmapCopier mapViewBitmapCopier,
             IconRenderer iconRenderer, DifficultyAndTerrainPainter difficultyAndTerrainPainter,
-            NameFormatter nameFormatter) {
+            NameFormatter nameFormatter, Resources resources, RatingsArray ratingsArray) {
         final TextView textViewName = (TextView)activity.findViewById(R.id.gcname);
         final NameViewer gcName = new NameViewer(textViewName, nameFormatter);
+
         final ImageView cacheTypeImageView = (ImageView)activity.findViewById(R.id.gcicon);
+        final Drawable[] ribbonDrawables = {
+                resources.getDrawable(R.drawable.ribbon_unselected_dark),
+                resources.getDrawable(R.drawable.ribbon_half_bright),
+                resources.getDrawable(R.drawable.ribbon_selected_bright)
+        };
+        final ImageView imageView = (ImageView)activity.findViewById(R.id.gc_difficulty);
+        final AttributeViewer ribbonImages = getImagesOnDifficulty(ribbonDrawables, imageView,
+                ratingsArray);
+
+        final AttributeViewer gcDifficulty = new LabelledAttributeViewer((TextView)activity
+                .findViewById(R.id.gc_text_difficulty), ribbonImages);
+
         return new GeocacheViewer(radarView, activity, gcName, cacheTypeImageView, gcDifficulty,
                 gcTerrain, gcContainer, iconOverlayFactory, mapViewBitmapCopier, iconRenderer,
                 difficultyAndTerrainPainter);

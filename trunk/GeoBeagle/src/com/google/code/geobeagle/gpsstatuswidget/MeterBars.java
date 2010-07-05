0851b411774096fc26ead571a6ed3676272a6493
Stephen Ng <stephen@theleengs.com>
2010-07-05 14:21:36 -0700

    Uninject LocationViewer and MeterBars.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/gpsstatuswidget/MeterBars.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/gpsstatuswidget/MeterBars.java
@@ -17,9 +17,6 @@ package com.google.code.geobeagle.gpsstatuswidget;
  * Displays the accuracy (graphically) and azimuth of the gps.
  */
 
-import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule.LocationViewer;
-import com.google.inject.Inject;
-
 import android.graphics.Color;
 import android.widget.TextView;
 
@@ -27,8 +24,7 @@ class MeterBars {
     private final TextView mBarsAndAzimuth;
     private final MeterFormatter mMeterFormatter;
 
-    @Inject
-    MeterBars(@LocationViewer TextView textView, MeterFormatter meterFormatter) {
+    MeterBars(TextView textView, MeterFormatter meterFormatter) {
         mBarsAndAzimuth = textView;
         mMeterFormatter = meterFormatter;
     }

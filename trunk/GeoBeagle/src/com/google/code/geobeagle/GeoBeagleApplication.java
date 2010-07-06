4037be70456395d36245bb7d122cd9fc330a7155
Stephen Ng <stephen@theleengs.com>
2010-07-06 10:45:47 -0700

    Move scoping out of XmlimportModule, remove now-empty module.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/GeoBeagleApplication.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/GeoBeagleApplication.java
@@ -22,7 +22,6 @@ import com.google.code.geobeagle.bcaching.BCachingModule;
 import com.google.code.geobeagle.database.DatabaseModule;
 import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule;
 import com.google.code.geobeagle.location.LocationModule;
-import com.google.code.geobeagle.xmlimport.XmlimportModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -66,7 +65,6 @@ public class GeoBeagleApplication extends GuiceApplication {
         modules.add(new LocationModule());
         modules.add(new ModelModule());
         modules.add(new GpsStatusWidgetModule());
-        modules.add(new XmlimportModule());     // +3.6 seconds (7.95)
         modules.add(new BCachingModule());
     }
 }

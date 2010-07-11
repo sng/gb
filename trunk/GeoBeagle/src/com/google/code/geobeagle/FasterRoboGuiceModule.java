aaff9e913384be67997b04d900e62e1882e59c70
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-07-11 15:58:39 +0000

    Remove unused services (11.7 seconds).
    git-svn-id: https://geobeagle.googlecode.com/svn@1582 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagle/src/com/google/code/geobeagle/FasterRoboGuiceModule.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/FasterRoboGuiceModule.java
@@ -15,9 +15,6 @@
 package com.google.code.geobeagle;
 
 import com.google.inject.Provider;
-import com.google.inject.TypeLiteral;
-import com.google.inject.matcher.AbstractMatcher;
-import com.google.inject.matcher.Matcher;
 import com.google.inject.matcher.Matchers;
 
 import roboguice.config.RoboGuiceModule;
@@ -34,23 +31,14 @@ import roboguice.inject.ViewListener;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.AlarmManager;
 import android.app.Application;
-import android.app.KeyguardManager;
-import android.app.NotificationManager;
-import android.app.SearchManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.wifi.WifiManager;
 import android.os.PowerManager;
-import android.os.Vibrator;
 import android.view.LayoutInflater;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 
 public class FasterRoboGuiceModule extends RoboGuiceModule {
 
@@ -66,7 +54,6 @@ public class FasterRoboGuiceModule extends RoboGuiceModule {
     @Override
     protected void configure() {
         // Sundry Android Classes
-        bind(SharedPreferences.class).toProvider(SharedPreferencesProvider.class);
         bind(Resources.class).toProvider(ResourcesProvider.class);
         bind(ContentResolver.class).toProvider(ContentResolverProvider.class);
 
@@ -78,30 +65,13 @@ public class FasterRoboGuiceModule extends RoboGuiceModule {
         // System Services
         bind(LocationManager.class).toProvider(
                 new SystemServiceProvider<LocationManager>(Context.LOCATION_SERVICE));
-        bind(WindowManager.class).toProvider(
-                new SystemServiceProvider<WindowManager>(Context.WINDOW_SERVICE));
         bind(LayoutInflater.class).toProvider(
                 new SystemServiceProvider<LayoutInflater>(Context.LAYOUT_INFLATER_SERVICE));
         bind(ActivityManager.class).toProvider(
                 new SystemServiceProvider<ActivityManager>(Context.ACTIVITY_SERVICE));
         bind(PowerManager.class).toProvider(
                 new SystemServiceProvider<PowerManager>(Context.POWER_SERVICE));
-        bind(AlarmManager.class).toProvider(
-                new SystemServiceProvider<AlarmManager>(Context.ALARM_SERVICE));
-        bind(NotificationManager.class).toProvider(
-                new SystemServiceProvider<NotificationManager>(Context.NOTIFICATION_SERVICE));
-        bind(KeyguardManager.class).toProvider(
-                new SystemServiceProvider<KeyguardManager>(Context.KEYGUARD_SERVICE));
-        bind(SearchManager.class).toProvider(
-                new SystemServiceProvider<SearchManager>(Context.SEARCH_SERVICE));
-        bind(Vibrator.class).toProvider(
-                new SystemServiceProvider<Vibrator>(Context.VIBRATOR_SERVICE));
-        bind(ConnectivityManager.class).toProvider(
-                new SystemServiceProvider<ConnectivityManager>(Context.CONNECTIVITY_SERVICE));
-        bind(WifiManager.class).toProvider(
-                new SystemServiceProvider<WifiManager>(Context.WIFI_SERVICE));
-        bind(InputMethodManager.class).toProvider(
-                new SystemServiceProvider<InputMethodManager>(Context.INPUT_METHOD_SERVICE));
+
 
         // Context Scope bindings
         bindScope(ContextScoped.class, contextScope);

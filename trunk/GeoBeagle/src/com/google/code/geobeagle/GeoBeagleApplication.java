ae1d184a7a340832a7d693206d0574f62bc99f45
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-07-11 15:47:36 +0000

    Clean up what's left of FieldnotesModule.
    git-svn-id: https://geobeagle.googlecode.com/svn@1553 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagle/src/com/google/code/geobeagle/GeoBeagleApplication.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/GeoBeagleApplication.java
@@ -17,7 +17,6 @@ package com.google.code.geobeagle;
 import com.google.code.geobeagle.activity.cachelist.CacheListModule;
 import com.google.code.geobeagle.activity.cachelist.model.ModelModule;
 import com.google.code.geobeagle.activity.main.GeoBeagleModule;
-import com.google.code.geobeagle.activity.main.fieldnotes.FieldnotesModule;
 import com.google.code.geobeagle.activity.main.view.ViewModule;
 import com.google.code.geobeagle.bcaching.BCachingModule;
 import com.google.code.geobeagle.bcaching.communication.BCachingCommModule;
@@ -37,7 +36,6 @@ public class GeoBeagleApplication extends GuiceApplication {
     protected void addApplicationModules(List<Module> modules) {
         timing.start();
 //        Debug.startMethodTracing("dmtrace", 32 * 1024 * 1024);
-        modules.add(new FieldnotesModule());   // +2.1 seconds (13.1 -- 11.5, 10.5, 10.7, 10.7)
         modules.add(new GeoBeagleModule());      // +1 second (11.0)
         modules.add(new GeoBeaglePackageModule());
         modules.add(new ViewModule());        // ?? (12.7)

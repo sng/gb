dcca5f2690d2407225e513579b36eed690cb3cee
stephen5.ng <stephen5.ng@eab1e41a-c3b7-11dd-b16b-519e865567fa>
2010-07-11 15:37:19 +0000

    Remove robot legs.
    git-svn-id: https://geobeagle.googlecode.com/svn@1474 eab1e41a-c3b7-11dd-b16b-519e865567fa

--- trunk/GeoBeagle/di/com/google/code/geobeagle/activity/cachelist/CacheListActivity.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/activity/cachelist/CacheListActivity.java
@@ -17,9 +17,7 @@ package com.google.code.geobeagle.activity.cachelist;
 import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetDelegate;
 import com.google.code.geobeagle.gpsstatuswidget.InflatedGpsStatusWidget;
-import com.google.code.geobeagle.gpsstatuswidget.GpsStatusWidgetModule.CacheList;
 import com.google.inject.Injector;
-import com.google.inject.Key;
 
 import roboguice.activity.GuiceListActivity;
 
@@ -63,10 +61,10 @@ public class CacheListActivity extends GuiceListActivity {
         super.onCreate(savedInstanceState);
         Log.d("GeoBeagle", "CacheListActivity onCreate");
         final Injector injector = this.getInjector();
-        final InflatedGpsStatusWidget inflatedGpsStatusWidget = injector.getInstance(Key.get(
-                InflatedGpsStatusWidget.class, CacheList.class));
-        final GpsStatusWidgetDelegate gpsStatusWidgetDelegate = injector.getInstance(Key.get(
-                GpsStatusWidgetDelegate.class, CacheList.class));
+        final InflatedGpsStatusWidget inflatedGpsStatusWidget = injector
+                .getInstance(InflatedGpsStatusWidget.class);
+        final GpsStatusWidgetDelegate gpsStatusWidgetDelegate = injector
+                .getInstance(GpsStatusWidgetDelegate.class);
         
         inflatedGpsStatusWidget.setDelegate(gpsStatusWidgetDelegate);
 

92bb730a3c11e06888cff4a0cd5d552b3e8723ce
Stephen Ng <stephen@theleengs.com>
2010-08-03 22:44:52 -0700

    Make details a full activity.

---
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/details/DetailsActivity.java
@@ -0,0 +1,41 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
+package com.google.code.geobeagle.activity.details;
+
+import com.google.code.geobeagle.R;
+import com.google.inject.Inject;
+
+import roboguice.activity.GuiceActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class DetailsActivity extends GuiceActivity {
+
+    @Inject
+    private DetailsWebView detailsWebView;
+    public static final String INTENT_EXTRA_GEOCACHE_SOURCE = "geocache_source";
+    public static final String INTENT_EXTRA_GEOCACHE_ID = "geocache_id";
+    public static final String INTENT_EXTRA_GEOCACHE_NAME = "geocache_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+        setTitle(detailsWebView.loadDetails((WebView)findViewById(R.id.cache_details), getIntent()));
+    }
+
+}

6634a7d8a5e5a0c3890f2edc67ffa0ceb1b9e764
Stephen Ng <stephen@theleengs.com>
2010-08-31 19:58:23 -0700

    random logging

--- trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/ImportCacheActions.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/ImportCacheActions.java
@@ -21,6 +21,7 @@ import com.google.inject.Inject;
 import roboguice.inject.ContextScoped;
 
 import android.os.PowerManager.WakeLock;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -120,6 +121,7 @@ public class ImportCacheActions implements ICachePersisterFacade {
 
     @Override
     public void symbol(String text) {
+        Log.d("GeoBeagle", "ImportCacheActions: SYMBOL: " + text);
         mCacheTagWriter.symbol(text);
     }
 

2da170c91ca82f4bfa000d0f9594853adb86e962
Stephen Ng <stephen@theleengs.com>
2010-10-31 19:45:59 -0700

    Pull ImportThreadHelper to separate file.

--- trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/ImportThreadDelegate.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/ImportThreadDelegate.java
@@ -17,74 +17,21 @@ package com.google.code.geobeagle.xmlimport;
 import com.google.code.geobeagle.ErrorDisplayer;
 import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.activity.cachelist.presenter.CacheListRefresh.UpdateFlag;
-import com.google.code.geobeagle.bcaching.BCachingModule;
 import com.google.code.geobeagle.bcaching.preferences.BCachingStartTime;
 import com.google.code.geobeagle.cachedetails.FileDataVersionChecker;
 import com.google.code.geobeagle.cachedetails.FileDataVersionWriter;
 import com.google.code.geobeagle.database.DbFrontend;
 import com.google.code.geobeagle.xmlimport.gpx.GpxAndZipFiles;
 import com.google.code.geobeagle.xmlimport.gpx.GpxAndZipFiles.GpxFilesAndZipFilesIter;
-import com.google.code.geobeagle.xmlimport.gpx.IGpxReader;
 
 import org.xmlpull.v1.XmlPullParserException;
 
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ImportThreadDelegate {
-
-    public static class ImportThreadHelper {
-        private final GpxLoader mGpxLoader;
-        private boolean mHasFiles;
-        private final MessageHandlerInterface mMessageHandler;
-        private final OldCacheFilesCleaner mOldCacheFilesCleaner;
-        private final GeoBeagleEnvironment mGeoBeagleEnvironment;
-        private final SharedPreferences mSharedPreferences;
-
-        public ImportThreadHelper(GpxLoader gpxLoader, MessageHandlerInterface messageHandler,
-                OldCacheFilesCleaner oldCacheFilesCleaner,
-                SharedPreferences sharedPreferences,
-                GeoBeagleEnvironment geoBeagleEnvironment) {
-            mGpxLoader = gpxLoader;
-            mMessageHandler = messageHandler;
-            mHasFiles = false;
-            mOldCacheFilesCleaner = oldCacheFilesCleaner;
-            mSharedPreferences = sharedPreferences;
-            mGeoBeagleEnvironment = geoBeagleEnvironment;
-        }
-
-        public void cleanup() {
-            mMessageHandler.loadComplete();
-        }
-
-        public void end() throws ImportException {
-            mGpxLoader.end();
-            if (!mHasFiles
-                    && mSharedPreferences.getString(BCachingModule.BCACHING_USERNAME, "").length() == 0)
-                throw new ImportException(R.string.error_no_gpx_files, mGeoBeagleEnvironment
-                        .getImportFolder());
-        }
-
-        public boolean processFile(IGpxReader gpxReader) throws XmlPullParserException, IOException {
-            String filename = gpxReader.getFilename();
-
-            mHasFiles = true;
-            mGpxLoader.open(filename, gpxReader.open());
-            return mGpxLoader.load();
-        }
-
-        public void start() {
-            mOldCacheFilesCleaner.clean();
-            mGpxLoader.start();
-        }
-
-        public void startBCachingImport() {
-            mMessageHandler.startBCachingImport();
-        }
-    }
 
     private final ErrorDisplayer mErrorDisplayer;
     private final GpxAndZipFiles mGpxAndZipFiles;

2e079620dd2984073b31f730a304f338701f9c4c
Stephen Ng <stephen@theleengs.com>
2011-01-28 19:25:57 -0800

    whitespace and copyright for MessageHandler.

--- trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/MessageHandler.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/MessageHandler.java
@@ -1,3 +1,17 @@
+/*
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
 package com.google.code.geobeagle.xmlimport;
 
 import com.google.code.geobeagle.activity.cachelist.presenter.CacheListRefresh;
@@ -138,5 +152,4 @@ public class MessageHandler extends Handler implements MessageHandlerInterface {
     public void startBCachingImport() {
         sendEmptyMessage(MessageHandler.MSG_BCACHING_IMPORT);
     }
-
 }

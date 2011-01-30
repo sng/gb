ef43a506af4f172c93b27a940bc7ea05440908d3
Stephen Ng <stephen@theleengs.com>
2011-01-30 08:55:36 -0800

    remove import thread wrapper

--- trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/GpxImporter.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/xmlimport/GpxImporter.java
@@ -19,54 +19,75 @@ import com.google.code.geobeagle.activity.cachelist.Pausable;
 import com.google.code.geobeagle.activity.cachelist.actions.menu.Abortable;
 import com.google.code.geobeagle.activity.cachelist.presenter.GeocacheListPresenter;
 import com.google.code.geobeagle.activity.main.fieldnotes.Toaster;
+import com.google.code.geobeagle.xmlimport.ImportThread.ImportThreadFactory;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
+import android.util.Log;
 import android.widget.Toast;
 
 public class GpxImporter implements Abortable {
 
-    private final ImportThreadWrapper mImportThreadWrapper;
     private final MessageHandler mMessageHandler;
     private final Toaster mToaster;
     private final Pausable mGeocacheListPresenter;
     private final Aborter mAborter;
+    private final ImportThreadFactory mImportThreadFactory;
+    private ImportThread mImportThread;
 
     GpxImporter(GeocacheListPresenter geocacheListPresenter,
-            ImportThreadWrapper importThreadWrapper,
             MessageHandler messageHandler,
             Toaster toaster,
-            Aborter aborter) {
-        mImportThreadWrapper = importThreadWrapper;
+            Aborter aborter,
+            ImportThreadFactory importThreadFactory) {
         mMessageHandler = messageHandler;
         mToaster = toaster;
         mGeocacheListPresenter = geocacheListPresenter;
         mAborter = aborter;
+        mImportThreadFactory = importThreadFactory;
     }
 
     @Inject
     GpxImporter(Injector injector) {
         mAborter = injector.getInstance(Aborter.class);
-        mImportThreadWrapper = injector.getInstance(ImportThreadWrapper.class);
         mMessageHandler = injector.getInstance(MessageHandler.class);
         mToaster = injector.getInstance(Toaster.class);
         mGeocacheListPresenter = injector.getInstance(GeocacheListPresenter.class);
+        mImportThreadFactory = injector.getInstance(ImportThreadFactory.class);
     }
 
     @Override
     public void abort() {
         mMessageHandler.abortLoad();
         mAborter.abort();
-        if (mImportThreadWrapper.isAlive()) {
-            mImportThreadWrapper.join();
+        if (isAlive()) {
+            join();
             mToaster.toast(R.string.import_canceled, Toast.LENGTH_SHORT);
         }
+    }
+
+    boolean isAlive() {
+        if (mImportThread != null)
+            return mImportThread.isAliveHack();
+        return false;
+    }
+
+    void join() {
+        if (mImportThread != null)
+            try {
+                while (!isAlive()) {
+                    Log.d("GeoBeagle", "Sleeping while gpx import completes");
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                // Ignore; we are aborting anyway.
+            }
     }
 
     public void importGpxs() {
         mGeocacheListPresenter.onPause();
 
-        mImportThreadWrapper.open();
-        mImportThreadWrapper.start();
+        mImportThread = mImportThreadFactory.create();
+        mImportThread.start();
     }
 }

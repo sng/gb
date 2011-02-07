ba7592ecb4b1a4e970d270c3d337bd205f8a8c77
Stephen Ng <stephen@theleengs.com>
2011-02-06 18:24:32 -0800

    Move ProgressDialogWrapper to new file.
    Conflicts:
    
    	trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/GpxImporterDI.java
    	trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/MessageHandler.java

--- trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/ProgressDialogWrapper.java
+++ trunk/GeoBeagle/di/com/google/code/geobeagle/xmlimport/ProgressDialogWrapper.java
@@ -1,17 +1,3 @@
-/*
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- **
- **     http://www.apache.org/licenses/LICENSE-2.0
- **
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- */
-
 package com.google.code.geobeagle.xmlimport;
 
 import com.google.inject.Inject;
@@ -20,9 +6,7 @@ import com.google.inject.Provider;
 import android.app.ProgressDialog;
 import android.content.Context;
 
-public class GpxImporterDI {
-    // Wrapper so that containers can follow the "constructors do no work" rule.
-    public static class ProgressDialogWrapper {
+public class ProgressDialogWrapper {
         private final Provider<Context> mContextProvider;
         private ProgressDialog mProgressDialog;
 
@@ -45,4 +29,3 @@ public class GpxImporterDI {
 //            mProgressDialog.setCancelable(true);
         }
     }
-}

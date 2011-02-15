a5f41602b8600ccdf333f247a2d77315694fd5ed
Stephen Ng <stephen@theleengs.com>
2011-02-15 08:41:44 -0800

    fix test

--- trunk/GeoBeagleTest/test/com/google/code/geobeagle/activity/cachelist/GeocacheListControllerTest.java
+++ trunk/GeoBeagleTest/test/com/google/code/geobeagle/activity/cachelist/GeocacheListControllerTest.java
@@ -50,31 +50,34 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 })
 public class GeocacheListControllerTest extends GeoBeagleTest {
     private AbortState abortState;
+    private ContextMenu contextMenu;
+    private CacheListRefresh cacheListRefresh;
+    private AdapterContextMenuInfo adapterContextMenuInfo;
 
     @Before
     public void setUp() {
         abortState = PowerMock.createMock(AbortState.class);
+        adapterContextMenuInfo = PowerMock.createMock(AdapterContextMenuInfo.class);
+        cacheListRefresh = PowerMock.createMock(CacheListRefresh.class);
+        contextMenu = PowerMock.createMock(ContextMenu.class);
     }
-
 
     @Test
     public void testCacheListOnCreateContextMenuListener() {
-        ContextMenu menu = PowerMock.createMock(ContextMenu.class);
-        AdapterContextMenuInfo menuInfo = PowerMock.createMock(AdapterContextMenuInfo.class);
         GeocacheVectors geocacheVectors = PowerMock.createMock(GeocacheVectors.class);
         GeocacheVector geocacheVector = PowerMock.createMock(GeocacheVector.class);
 
         expect(geocacheVectors.get(11)).andReturn(geocacheVector);
         expect(geocacheVector.getId()).andReturn("GC123");
-        expect(menu.setHeaderTitle("GC123")).andReturn(menu);
-        expect(menu.add(0, GeocacheListController.MENU_VIEW, 0, "View")).andReturn(null);
-        expect(menu.add(0, GeocacheListController.MENU_EDIT, 1, "Edit")).andReturn(null);
-        expect(menu.add(0, GeocacheListController.MENU_DELETE, 2, "Delete")).andReturn(null);
+        expect(contextMenu.setHeaderTitle("GC123")).andReturn(contextMenu);
+        expect(contextMenu.add(0, GeocacheListController.MENU_VIEW, 0, "View")).andReturn(null);
+        expect(contextMenu.add(0, GeocacheListController.MENU_EDIT, 1, "Edit")).andReturn(null);
+        expect(contextMenu.add(0, GeocacheListController.MENU_DELETE, 2, "Delete")).andReturn(null);
 
         PowerMock.replayAll();
-        menuInfo.position = 12;
-        new CacheListOnCreateContextMenuListener(geocacheVectors).onCreateContextMenu(menu, null,
-                menuInfo);
+        adapterContextMenuInfo.position = 12;
+        new CacheListOnCreateContextMenuListener(geocacheVectors).onCreateContextMenu(contextMenu, null,
+                adapterContextMenuInfo);
         PowerMock.verifyAll();
     }
 
@@ -102,7 +105,6 @@ public class GeocacheListControllerTest extends GeoBeagleTest {
 
     @Test
     public void testOnCreateContextMenu() {
-        ContextMenu contextMenu = PowerMock.createMock(ContextMenu.class);
         MenuItem menuItem = PowerMock.createMock(MenuItem.class);
         GeocacheVectors geocacheVectors = PowerMock.createMock(GeocacheVectors.class);
         GeocacheVector geocacheVector = PowerMock.createMock(GeocacheVector.class);
@@ -202,8 +204,6 @@ public class GeocacheListControllerTest extends GeoBeagleTest {
 
     @Test
     public void testOnResume() {
-        CacheListRefresh cacheListRefresh = PowerMock.createMock(CacheListRefresh.class);
-
         cacheListRefresh.forceRefresh();
 
         PowerMock.replayAll();

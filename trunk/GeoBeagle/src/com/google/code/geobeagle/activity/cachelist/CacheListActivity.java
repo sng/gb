c5ecea85ee6c2a26dca27d542f4719a5f7db8f4f
Stephen Ng <stephen@theleengs.com>
2011-02-27 21:07:54 -0800

    Handle configuration change (for honeycomb).

--- trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListActivity.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListActivity.java
@@ -30,6 +30,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.util.Log;
@@ -46,6 +47,12 @@ public class CacheListActivity extends GuiceListActivity {
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         return mCacheListDelegate.onContextItemSelected(item) || super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.cache_list);
     }
 
     @Override

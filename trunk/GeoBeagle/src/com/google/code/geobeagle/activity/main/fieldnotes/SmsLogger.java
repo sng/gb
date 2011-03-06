b8cda91ee2a6a8f4fca99a17d288415d9bf4ca8c
Stephen Ng <stephen@theleengs.com>
2011-03-06 09:30:31 -0800

    rename fields, sort

--- trunk/GeoBeagle/src/com/google/code/geobeagle/activity/main/fieldnotes/SmsLogger.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/main/fieldnotes/SmsLogger.java
@@ -23,17 +23,17 @@ import android.content.Context;
 import android.content.Intent;
 
 public class SmsLogger implements ICacheLogger {
-    private final Context mContext;
-    private final FieldnoteStringsFVsDnf mFieldNoteStringsFoundVsDnf;
-    private final ErrorDisplayer mErrorDisplayer;
+    private final Context context;
+    private final ErrorDisplayer errorDisplayer;
+    private final FieldnoteStringsFVsDnf fieldNoteStringsFoundVsDnf;
 
     @Inject
     public SmsLogger(FieldnoteStringsFVsDnf fieldnoteStringsFVsDnf,
             Context context,
             ErrorDisplayer errorDisplayer) {
-        mFieldNoteStringsFoundVsDnf = fieldnoteStringsFVsDnf;
-        mContext = context;
-        mErrorDisplayer = errorDisplayer;
+        this.fieldNoteStringsFoundVsDnf = fieldnoteStringsFVsDnf;
+        this.context = context;
+        this.errorDisplayer = errorDisplayer;
     }
 
     @Override
@@ -41,13 +41,13 @@ public class SmsLogger implements ICacheLogger {
         Intent sendIntent = new Intent(Intent.ACTION_VIEW);
         sendIntent.putExtra("address", "41411");
         sendIntent.putExtra("sms_body",
-                mFieldNoteStringsFoundVsDnf.getString(R.array.fieldnote_code, dnf) + geocacheId
+                fieldNoteStringsFoundVsDnf.getString(R.array.fieldnote_code, dnf) + geocacheId
                         + " " + logText);
         sendIntent.setType("vnd.android-dir/mms-sms");
         try {
-            mContext.startActivity(sendIntent);
+            context.startActivity(sendIntent);
         } catch (ActivityNotFoundException e) {
-            mErrorDisplayer.displayError(R.string.sms_fail);
+            errorDisplayer.displayError(R.string.sms_fail);
         }
     }
 }

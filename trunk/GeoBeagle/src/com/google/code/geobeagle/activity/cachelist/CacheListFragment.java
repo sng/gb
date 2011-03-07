9116baaaef049ec8e3bd936a6510e40c429b58e6
Stephen Ng <stephen@theleengs.com>
2011-03-07 11:31:30 -0800

    unused imports

--- trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListFragment.java
+++ trunk/GeoBeagle/src/com/google/code/geobeagle/activity/cachelist/CacheListFragment.java
@@ -14,28 +14,22 @@
 
 package com.google.code.geobeagle.activity.cachelist;
 
-import com.google.code.geobeagle.Geocache;
 import com.google.code.geobeagle.R;
 import com.google.code.geobeagle.activity.cachelist.model.GeocacheVector;
 import com.google.code.geobeagle.activity.cachelist.model.GeocacheVectors;
 import com.google.code.geobeagle.activity.compass.CompassFragment;
 import com.google.inject.Injector;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.ListFragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 public class CacheListFragment extends ListFragment {
     @Override
